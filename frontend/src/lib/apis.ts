346a0727b314e2087f454f8295aecc93e56ab0e9
slomg <74960133+slomg@users.noreply.github.com>
2024-01-02 16:08:22 +0200

    Fixed cookies not being sent from the server

--- frontend/src/lib/apis.ts
+++ frontend/src/lib/apis.ts
@@ -1,14 +1,34 @@
 import {
     GameRequestsApi,
+    RequestContext,
     Configuration,
+    FetchParams,
     SettingsApi,
     ProfileApi,
     AuthApi,
 } from "../client";
 
+/**
+ * Update the cookies when a request is sent from the server
+ */
+async function addServerCookies(
+    context: RequestContext
+): Promise<FetchParams | void> {
+    if (typeof window !== "undefined") return;
+
+    const { cookies } = await import("next/headers");
+
+    const headers = new Headers(context.init.headers);
+    headers.set("Cookie", cookies().toString());
+    context.init.headers = headers;
+
+    return context;
+}
+
 export const apiConfig = new Configuration({
     basePath: process.env.NEXT_PUBLIC_API_URL,
     credentials: "include",
+    middleware: [{ pre: addServerCookies }],
 });
 
 export const gameRequestApi = new GameRequestsApi(apiConfig);
