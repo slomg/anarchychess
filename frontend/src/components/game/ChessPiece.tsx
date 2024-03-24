a605ec72fe246f9e830ce1cec724b236a27bdcee
slomg <74960133+slomg@users.noreply.github.com>
2024-03-24 15:58:20 +0200

    new piece dragging system that doesn't suck

--- frontend/src/components/game/ChessPiece.tsx
+++ frontend/src/components/game/ChessPiece.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useRef, useState } from "react";
+import { memo, useRef, useState, MouseEvent as ReactMouseEvent } from "react";
 
 import { useBoardSize, usePiece, useViewingFrom } from "@/hooks/useChess";
 import styles from "./ChessPiece.module.scss";
@@ -10,36 +10,10 @@ export const ChessPiece = ({ id }: { id: string }) => {
     const viewingFrom = useViewingFrom();
     const pieceRef = useRef<HTMLDivElement>(null);
 
-    const [dragPosition, setDragPosition] = useState<Point>([0, 0]);
-    const [startingPosition, setStartingPosition] = useState<Point>([0, 0]);
-    const [isDragging, setIsDragging] = useState<boolean>(false);
-
-    useEffect(() => {
-        if (!isDragging) return;
-
-        function disableDragging() {
-            document.removeEventListener("mousemove", onMove);
-            setDragPosition([0, 0]);
-            setIsDragging(false);
-        }
-
-        function onMove(event: MouseEvent) {
-            event.preventDefault();
-            //if (!isDragging) return;
-
-            const x = event.clientX;
-            const y = event.clientY;
-
-            setDragPosition([x - startingPosition[0], y - startingPosition[1]]);
-        }
-
-        document.addEventListener("mousemove", onMove);
-        document.addEventListener("mouseup", disableDragging);
-
-        return disableDragging;
-    }, [isDragging, startingPosition]);
+    const [draggingOffset, setDraggingOffset] = useState<Point>([0, 0]);
 
     if (!piece) return;
+
     const { position, pieceType, color } = piece;
 
     const boardSize = boardWidth * boardHeight;
@@ -52,21 +26,44 @@ export const ChessPiece = ({ id }: { id: string }) => {
     const physicalX = x * boardWidth * boardHeight;
     const physicalY = y * boardSize;
 
+    /**
+     * Start piece dragging
+     */
+    function startDragging(event: ReactMouseEvent): void {
+        // calculate the initial position relative to the mouse
+        const startX = event.clientX;
+        const startY = event.clientY;
+
+        // calculate the new offset when the mouse moves
+        function handleMouseMove(event: MouseEvent): void {
+            const x = event.clientX - startX;
+            const y = event.clientY - startY;
+            setDraggingOffset([x, y]);
+        }
+
+        // reset the event listeners and the dragging offset
+        function stopDragging() {
+            setDraggingOffset([0, 0]);
+            window.removeEventListener("mousemove", handleMouseMove);
+            window.removeEventListener("mouseup", stopDragging);
+        }
+
+        // add event listeners for mouse movement and release
+        window.addEventListener("mousemove", handleMouseMove);
+        window.addEventListener("mouseup", stopDragging);
+    }
+
     return (
         <div
             data-testid="piece"
             className={styles.piece}
             ref={pieceRef}
-            draggable={false}
-            onMouseDown={(event) => {
-                setStartingPosition([event.clientX, event.clientY]);
-                setIsDragging(true);
-            }}
+            onMouseDown={startDragging}
             style={{
                 backgroundImage: `url("/assets/pieces/${pieceType}-${color}.png")`,
-                transform: `translate(${physicalX + dragPosition[0]}%, ${
-                    physicalY + dragPosition[1]
-                }%)`,
+                transform: `translate(${physicalX}%, ${physicalY}%)`,
+                left: draggingOffset[0],
+                top: draggingOffset[1],
             }}
         />
     );
