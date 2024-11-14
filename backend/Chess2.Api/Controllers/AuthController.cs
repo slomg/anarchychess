506f4d85062f8cbcad9ab443d62315693da7a235
slomg <74960133+slomg@users.noreply.github.com>
2024-11-14 14:16:40 +0200

    Correct response type docs for refresh and login

--- backend/Chess2.Api/Controllers/AuthController.cs
+++ backend/Chess2.Api/Controllers/AuthController.cs
@@ -28,7 +28,7 @@ public class AuthController(ILogger<AuthController> logger, IAuthService authSer
     }
 
     [HttpPost("login")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType<Tokens>(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IResult> Login([FromBody] UserLogin userAuth, CancellationToken cancellation)
@@ -48,6 +48,7 @@ public class AuthController(ILogger<AuthController> logger, IAuthService authSer
     [HttpPost("refresh")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [Authorize("RefreshToken")]
     public async Task<IResult> Refresh(CancellationToken cancellation)
     {
