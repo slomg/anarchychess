d4a4a6175bbf8d663ba9f69cf3ac66d4d2747d73
slomg <74960133+slomg@users.noreply.github.com>
2024-11-15 16:00:48 +0200

    Remove test exception  from register

--- backend/Chess2.Api/Controllers/AuthController.cs
+++ backend/Chess2.Api/Controllers/AuthController.cs
@@ -19,7 +19,6 @@ public class AuthController(ILogger<AuthController> logger, IAuthService authSer
     [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<IResult> Register([FromBody] UserIn userIn, CancellationToken cancellation)
     {
-        throw new Exception();
         var result = await _authService.RegisterUserAsync(userIn, cancellation);
         return result.Match((value) =>
         {
