19975da175adb7cc9fe4dc04c5de60f66a6ca4da
slomg <74960133+slomg@users.noreply.github.com>
2024-11-17 11:56:55 +0200

    Make users required

--- backend/Chess2.Api/Models/Chess2DbContext.cs
+++ backend/Chess2.Api/Models/Chess2DbContext.cs
@@ -5,5 +5,5 @@ namespace Chess2.Api.Models;
 
 public class Chess2DbContext(DbContextOptions<Chess2DbContext> options) : DbContext(options)
 {
-    public DbSet<User> Users { get; set; }
+    public required DbSet<User> Users { get; set; }
 }
