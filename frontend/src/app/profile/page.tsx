c047d111d2ad86dfb9d892e4f60667b397ecd9f4
slomg <74960133+slomg@users.noreply.github.com>
2024-11-22 15:14:20 +0200

    Add comment

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import withAuth from "@/hocs/withAuth";
 
+// redirect the user to their profile page
 const RedirectUserPage = withAuth(async ({ profile }) => {
     redirect(`/profile/${profile.username}`);
 });
