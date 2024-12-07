7efe9843035a48de625b16baf19c1eac622b9ed2
slomg <74960133+slomg@users.noreply.github.com>
2024-12-07 16:34:58 +0200

    Verify arguments with fluentassertion

---
+++ backend/Tests/Chess2.Api.TestInfrastructure/NSubtituteExtenstion/ArgEx.cs
@@ -0,0 +1,12 @@
+﻿using NSubstitute.Core.Arguments;
+
+namespace Chess2.Api.TestInfrastructure.NSubtituteExtenstion;
+
+public static class ArgEx
+{
+    /// <summary>
+    /// Verifies all fluent assertions in the provided action succeed for the argument
+    /// </summary>
+    public static ref T FluentAssert<T>(Action<T> assertion) =>
+        ref ArgumentMatcher.Enqueue(new FluentAssertionArgumentMatcher<T>(assertion))!;
+}
---
+++ backend/Tests/Chess2.Api.TestInfrastructure/NSubtituteExtenstion/AssertionArgumentMatcher.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions.Execution;
+using NSubstitute.Core;
+using NSubstitute.Core.Arguments;
+
+namespace Chess2.Api.TestInfrastructure.NSubtituteExtenstion;
+
+public class FluentAssertionArgumentMatcher<T>(Action<T> assertion) : IArgumentMatcher<T>, IDescribeNonMatches
+{
+    private readonly Action<T> _assertion = assertion;
+    private string _failedExpectations = string.Empty;
+
+    public string DescribeFor(object? argument) => _failedExpectations;
+
+    /// <summary>
+    /// Use fluentassertion to validate the assertion
+    /// </summary>
+    public bool IsSatisfiedBy(T? value)
+    {
+        if (value is null)
+        {
+            _failedExpectations = "Value is null";
+            return false;
+        }
+
+        using var scope = new AssertionScope();
+        _assertion(value);
+        _failedExpectations = string.Join(Environment.NewLine, scope.Discard());
+
+        return _failedExpectations.Length == 0;
+    }
+
+}
