3f74ad202fea1a3685f129d8ad931d753a89033b
slomg <74960133+slomg@users.noreply.github.com>
2024-12-14 16:38:59 +0200

    Redis test container

--- backend/Tests/Chess2.Api.TestInfrastructure/Chess2WebApplicationFactory.cs
+++ backend/Tests/Chess2.Api.TestInfrastructure/Chess2WebApplicationFactory.cs
@@ -10,7 +10,9 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Npgsql;
 using Refit;
 using Respawn;
+using StackExchange.Redis;
 using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
 
 namespace Chess2.Api.TestInfrastructure;
 
@@ -23,8 +25,13 @@ public class Chess2WebApplicationFactory : WebApplicationFactory<Program>, IAsyn
         .WithPassword("postgres")
         .Build();
 
+    private readonly RedisContainer _redisContainer = new RedisBuilder()
+        .WithImage("docker.dragonflydb.io/dragonflydb/dragonfly")
+        .Build();
+
     private DbConnection _dbConnection = null!;
     private Respawner _respawner = null!;
+    private IDatabase _redisDb = null!;
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -34,6 +41,11 @@ public class Chess2WebApplicationFactory : WebApplicationFactory<Program>, IAsyn
             services.RemoveAll(typeof(DbContextOptions<Chess2DbContext>));
             services.AddDbContextPool<Chess2DbContext>(options =>
                 options.UseNpgsql(_dbContainer.GetConnectionString()).UseSnakeCaseNamingConvention()
+            );
+
+            services.RemoveAll(typeof(IConnectionMultiplexer));
+            services.AddSingleton<IConnectionMultiplexer>(
+                ConnectionMultiplexer.Connect(_redisContainer.GetConnectionString())
             );
         });
     }
@@ -62,20 +74,37 @@ public class Chess2WebApplicationFactory : WebApplicationFactory<Program>, IAsyn
     /// </summary>
     public IChess2Api CreateTypedClient() => RestService.For<IChess2Api>(CreateClient());
 
-    public Task ResetDatabaseAsync() => _respawner.ResetAsync(_dbConnection);
+    public async Task ResetDatabaseAsync()
+    {
+        await _respawner.ResetAsync(_dbConnection);
+        await _redisDb.ExecuteAsync("FLUSHDB");
+    }
 
     public async Task InitializeAsync()
     {
+        await _dbContainer.StartAsync();
+        await _redisContainer.StartAsync();
+
         await InitializeDbContainer();
         await InitializeRespawner();
+        InitializeRedisContainer();
     }
 
-    public new Task DisposeAsync() => _dbContainer.StopAsync();
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+        await _redisContainer.StopAsync();
+    }
+
+    private void InitializeRedisContainer()
+    {
+        using var scope = Services.CreateScope();
+        var redisConnection = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
+        _redisDb = redisConnection.GetDatabase();
+    }
 
     private async Task InitializeDbContainer()
     {
-        await _dbContainer.StartAsync();
-
         using var scope = Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<Chess2DbContext>();
         await dbContext.Database.EnsureCreatedAsync();
