910662c04067d120ece6e689842f6f91ea276589
slomg <74960133+slomg@users.noreply.github.com>
2025-05-16 18:48:03 +0300

    Fix auth page tests

--- frontend/src/components/authPage/__tests__/AuthPage.test.tsx
+++ frontend/src/components/authPage/__tests__/AuthPage.test.tsx
@@ -1,20 +1,23 @@
 import { render, screen } from "@testing-library/react";
 
-import SignupForm from "../SignupForm";
-import LoginForm from "../LoginForm";
 import AuthPage from "../AuthPage";
 
 describe("AuthPage", () => {
-    it("should render login form with link to signup", () => {
-        render(<AuthPage form={<LoginForm />} />);
-
-        expect(screen.queryByTestId("loginForm")).toBeInTheDocument();
-        expect(screen.queryByTestId("signupForm")).not.toBeInTheDocument();
+    it("should rendersthe logo text image", () => {
+        render(<AuthPage />);
+        const logoImg = screen.getByAltText(/logo/i);
+        expect(logoImg).toBeInTheDocument();
     });
 
-    it("should render signup form with link to login", () => {
-        render(<AuthPage form={<SignupForm />} />);
-        expect(screen.queryByTestId("signupForm")).toBeInTheDocument();
-        expect(screen.queryByTestId("loginForm")).not.toBeInTheDocument();
+    it("should render Google OAuth button", () => {
+        render(<AuthPage />);
+        expect(screen.getByText(/Continue with Google/i)).toBeInTheDocument();
+        expect(screen.getByAltText(/Google Icon/i)).toBeInTheDocument();
+    });
+
+    it("should render Discord OAuth button", () => {
+        render(<AuthPage />);
+        expect(screen.getByText(/Continue with Discord/i)).toBeInTheDocument();
+        expect(screen.getByAltText(/Discord Icon/i)).toBeInTheDocument();
     });
 });
---
+++ frontend/src/components/authPage/__tests__/OAuthButton.test.tsx
@@ -0,0 +1,66 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import OAuthButton from "../OAuthButton";
+import React from "react";
+import constants, { OAuthProvider } from "@/lib/constants";
+import { mockRouter } from "@/lib/testUtils/mocks";
+
+describe("OAuthButton", () => {
+    const icon = <span data-testid="icon">Icon</span>;
+    const loginText = "Login with Google";
+    const oauthProvider = OAuthProvider.GOOGLE;
+
+    it("should render the button with icon and text", () => {
+        render(
+            <OAuthButton
+                icon={icon}
+                loginText={loginText}
+                oauthProvider={oauthProvider}
+            />,
+        );
+        expect(screen.getByText(loginText)).toBeInTheDocument();
+        expect(screen.getByTestId("icon")).toBeInTheDocument();
+    });
+
+    it("should apply custom className", () => {
+        render(
+            <OAuthButton
+                icon={icon}
+                loginText={loginText}
+                oauthProvider={oauthProvider}
+                className="custom-class"
+            />,
+        );
+        const button = screen.getByRole("button");
+        expect(button.className).toContain("custom-class");
+    });
+
+    it("should call router.push with correct URL on click", () => {
+        const { push } = mockRouter();
+
+        render(
+            <OAuthButton
+                icon={icon}
+                loginText={loginText}
+                oauthProvider={oauthProvider}
+            />,
+        );
+        const button = screen.getByRole("button");
+        fireEvent.click(button);
+
+        expect(push).toHaveBeenCalledWith(`${constants.PATHS.OAUTH}google`);
+    });
+
+    it("should passe additional props to the button", () => {
+        render(
+            <OAuthButton
+                icon={icon}
+                loginText={loginText}
+                oauthProvider={oauthProvider}
+                data-testid="oauth-btn"
+                disabled
+            />,
+        );
+        const button = screen.getByTestId("oauth-btn");
+        expect(button).toBeDisabled();
+    });
+});
