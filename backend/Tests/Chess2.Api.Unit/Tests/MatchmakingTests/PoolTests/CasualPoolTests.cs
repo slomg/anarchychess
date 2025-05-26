4d2a91cca8f0d34e866f104257b7269fbdce672f
slomg <74960133+slomg@users.noreply.github.com>
2025-05-26 10:50:03 +0300

    Add casual pool tests

--- backend/Tests/Chess2.Api.Unit/Tests/MatchmakingTests/PoolTests/CasualPoolTests.cs
+++ backend/Tests/Chess2.Api.Unit/Tests/MatchmakingTests/PoolTests/CasualPoolTests.cs
@@ -1,12 +1,49 @@
 ﻿using Chess2.Api.Matchmaking.Services.Pools;
+using FluentAssertions;
 
 namespace Chess2.Api.Unit.Tests.MatchmakingTests.PoolTests;
 
-internal class CasualPoolTests : BasePoolTests<CasualMatchmakingPool>
+public class CasualPoolTests : BasePoolTests<CasualMatchmakingPool>
 {
-    private readonly CasualMatchmakingPool _pool = new();
+    protected override CasualMatchmakingPool Pool { get; } = new();
 
-    protected override CasualMatchmakingPool Pool => _pool;
+    protected override void AddSeek(string userId) => Pool.AddSeek(userId);
 
-    protected override void AddSeek(string userId) => _pool.AddSeek(userId);
+    [Fact]
+    public void CalculateMatches_returns_empty_list_when_no_seekers()
+    {
+        var matches = Pool.CalculateMatches();
+        matches.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void CalculateMatches_returns_all_pairs_with_even_number_of_seekers()
+    {
+        Pool.AddSeek("User1");
+        Pool.AddSeek("User2");
+        Pool.AddSeek("User3");
+        Pool.AddSeek("User4");
+
+        var matches = Pool.CalculateMatches();
+
+        matches.Should().HaveCount(2);
+        matches.Should().Contain(("User1", "User2"));
+        matches.Should().Contain(("User3", "User4"));
+
+        Pool.Seekers.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void CalculateMatches_leaves_last_seeker_unmatched_with_odd_number_of_seekers()
+    {
+        Pool.AddSeek("User1");
+        Pool.AddSeek("User2");
+        Pool.AddSeek("User3");
+
+        var matches = Pool.CalculateMatches();
+
+        matches.Should().ContainSingle().Which.Should().Be(("User1", "User2"));
+
+        Pool.Seekers.Should().ContainSingle().Which.Should().Be("User3");
+    }
 }
--- backend/Tests/Chess2.Api.Unit/Tests/MatchmakingTests/PoolTests/RatedPoolTests.cs
+++ backend/Tests/Chess2.Api.Unit/Tests/MatchmakingTests/PoolTests/RatedPoolTests.cs
@@ -9,26 +9,24 @@ namespace Chess2.Api.Unit.Tests.MatchmakingTests.PoolTests;
 public class RatedPoolTests : BasePoolTests<RatedMatchmakingPool>
 {
     private readonly GameSettings _settings;
-    private readonly RatedMatchmakingPool _pool;
-
-    protected override RatedMatchmakingPool Pool => _pool;
+    protected override RatedMatchmakingPool Pool { get; }
 
     public RatedPoolTests()
     {
         var settings = Fixture.Create<IOptions<AppSettings>>();
         _settings = settings.Value.Game;
-        _pool = new RatedMatchmakingPool(settings);
+        Pool = new RatedMatchmakingPool(settings);
     }
 
-    protected override void AddSeek(string userId) => _pool.AddSeek(userId, 1200);
+    protected override void AddSeek(string userId) => Pool.AddSeek(userId, 1200);
 
     [Fact]
     public void CalculateMatches_matches_within_range()
     {
-        _pool.AddSeek("user1", 1200);
-        _pool.AddSeek("user2", 1200 + _settings.StartingMatchRatingDifference);
+        Pool.AddSeek("user1", 1200);
+        Pool.AddSeek("user2", 1200 + _settings.StartingMatchRatingDifference);
 
-        var matches = _pool.CalculateMatches();
+        var matches = Pool.CalculateMatches();
 
         matches.Should().ContainSingle().Which.Should().BeEquivalentTo(("user1", "user2"));
     }
@@ -36,10 +34,10 @@ public class RatedPoolTests : BasePoolTests<RatedMatchmakingPool>
     [Fact]
     public void CalculateMatches_does_not_match_outside_range()
     {
-        _pool.AddSeek("user1", 1200);
-        _pool.AddSeek("user2", 1200 + _settings.StartingMatchRatingDifference + 1);
+        Pool.AddSeek("user1", 1200);
+        Pool.AddSeek("user2", 1200 + _settings.StartingMatchRatingDifference + 1);
 
-        var matches = _pool.CalculateMatches();
+        var matches = Pool.CalculateMatches();
 
         matches.Should().BeEmpty();
     }
@@ -47,38 +45,38 @@ public class RatedPoolTests : BasePoolTests<RatedMatchmakingPool>
     [Fact]
     public void CalculateMatches_increases_rating_range_after_waves_missed()
     {
-        _pool.AddSeek("user1", 1200);
-        _pool.AddSeek(
+        Pool.AddSeek("user1", 1200);
+        Pool.AddSeek(
             "user2",
             1200
                 + _settings.StartingMatchRatingDifference
                 + _settings.MatchRatingDifferenceGrowthPerWave
         );
         // First wave: no match
-        var matches1 = _pool.CalculateMatches();
+        var matches1 = Pool.CalculateMatches();
         matches1.Should().BeEmpty();
 
         // Second wave: range grows, now matches
-        var matches2 = _pool.CalculateMatches();
+        var matches2 = Pool.CalculateMatches();
         matches2.Should().ContainSingle().Which.Should().BeEquivalentTo(("user1", "user2"));
     }
 
     [Fact]
     public void CalculateMatches_prioritizes_old_seeks()
     {
-        _pool.AddSeek("user1", 1200);
-        _pool.AddSeek("user2", 1210);
-        _pool.AddSeek("user3", 1215);
+        Pool.AddSeek("user1", 1200);
+        Pool.AddSeek("user2", 1210);
+        Pool.AddSeek("user3", 1215);
 
         // match user 1 and user 2
-        var matches1 = _pool.CalculateMatches();
+        var matches1 = Pool.CalculateMatches();
         matches1.Should().ContainSingle().Which.Should().BeEquivalentTo(("user1", "user2"));
 
         // re-add the users
-        _pool.AddSeek("user1", 1200);
-        _pool.AddSeek("user2", 1210);
+        Pool.AddSeek("user1", 1200);
+        Pool.AddSeek("user2", 1210);
         // now user 3 has missed a wave, so it should be prioritized
-        var matches = _pool.CalculateMatches();
+        var matches = Pool.CalculateMatches();
 
         matches.Should().ContainSingle().Which.Should().BeEquivalentTo(("user3", "user2"));
     }
@@ -86,39 +84,39 @@ public class RatedPoolTests : BasePoolTests<RatedMatchmakingPool>
     [Fact]
     public void CalculateMatches_no_seekers_returns_empty()
     {
-        var matches = _pool.CalculateMatches();
+        var matches = Pool.CalculateMatches();
         matches.Should().BeEmpty();
     }
 
     [Fact]
     public void CalculateMatches_single_seeker_returns_no_matches()
     {
-        _pool.AddSeek("user1", 1500);
-        var matches = _pool.CalculateMatches();
+        Pool.AddSeek("user1", 1500);
+        var matches = Pool.CalculateMatches();
         matches.Should().BeEmpty();
     }
 
     [Fact]
     public void CalculateMatches_match_prefers_closer_rating_when_waves_missed_equal()
     {
-        _pool.AddSeek("user1", 1200);
-        _pool.AddSeek("user2", 1210);
-        _pool.AddSeek("user3", 1215);
+        Pool.AddSeek("user1", 1200);
+        Pool.AddSeek("user2", 1210);
+        Pool.AddSeek("user3", 1215);
 
         // Both user2 and user3 can match with user1, but user2 has closer rating difference
-        var matches = _pool.CalculateMatches();
+        var matches = Pool.CalculateMatches();
         matches.Should().ContainSingle().Which.Should().BeEquivalentTo(("user1", "user2"));
     }
 
     [Fact]
     public void CalculateMatches_handles_multiple_pairs()
     {
-        _pool.AddSeek("user1", 1200);
-        _pool.AddSeek("user2", 1205);
-        _pool.AddSeek("user3", 1210);
-        _pool.AddSeek("user4", 1195);
+        Pool.AddSeek("user1", 1200);
+        Pool.AddSeek("user2", 1205);
+        Pool.AddSeek("user3", 1210);
+        Pool.AddSeek("user4", 1195);
 
-        var matches = _pool.CalculateMatches();
+        var matches = Pool.CalculateMatches();
 
         // Should create two matches
         matches.Count.Should().Be(2);
@@ -130,18 +128,18 @@ public class RatedPoolTests : BasePoolTests<RatedMatchmakingPool>
     [Fact]
     public void CalculateMatches_removes_matched_users()
     {
-        _pool.AddSeek("user1", 1200);
-        _pool.AddSeek("user2", 1205);
-        _pool.AddSeek("user3", 1205);
+        Pool.AddSeek("user1", 1200);
+        Pool.AddSeek("user2", 1205);
+        Pool.AddSeek("user3", 1205);
 
-        _pool.SeekerCount.Should().Be(3);
+        Pool.SeekerCount.Should().Be(3);
 
-        var matches1 =_pool.CalculateMatches();
+        var matches1 = Pool.CalculateMatches();
         matches1.Should().ContainSingle();
 
-        _pool.Seekers.Should().ContainSingle().Which.Should().Be("user3");
+        Pool.Seekers.Should().ContainSingle().Which.Should().Be("user3");
 
-        var matches2 = _pool.CalculateMatches();
+        var matches2 = Pool.CalculateMatches();
         matches2.Should().BeEmpty();
     }
 }
