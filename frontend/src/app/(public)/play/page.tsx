23aea3f5c1a0032d995466ad24ae33924b76fa70
slomg <74960133+slomg@users.noreply.github.com>
2025-05-29 12:01:15 +0300

    Make sure chessboard is always centered on the screen

--- frontend/src/app/(public)/play/page.tsx
+++ frontend/src/app/(public)/play/page.tsx
@@ -6,23 +6,25 @@ export const metadata = { title: "Play - Chess 2" };
 const PlayPage = () => {
     return (
         <div
-            className="grid w-full max-w-full auto-cols-fr auto-rows-min items-center justify-center
-                gap-5 p-5 lg:auto-rows-fr lg:grid-cols-[auto_auto]"
+            className="grid w-full max-w-full auto-cols-fr auto-rows-min justify-center gap-5 p-5
+                lg:auto-rows-fr lg:grid-cols-[auto_auto]"
         >
-            <Chessboard
-                breakpoints={[
-                    {
-                        maxScreenSize: 768,
-                        paddingOffset: { width: 10, height: 110 },
-                    },
-                    {
-                        maxScreenSize: 1024,
-                        paddingOffset: { width: 200, height: 50 },
-                    },
-                ]}
-                defaultOffset={{ width: 626, height: 100 }}
-                className="justify-self-center"
-            />
+            <div className="flex md:max-h-screen">
+                <Chessboard
+                    breakpoints={[
+                        {
+                            maxScreenSize: 768,
+                            paddingOffset: { width: 10, height: 110 },
+                        },
+                        {
+                            maxScreenSize: 1024,
+                            paddingOffset: { width: 200, height: 50 },
+                        },
+                    ]}
+                    defaultOffset={{ width: 626, height: 100 }}
+                    className="m-auto"
+                />
+            </div>
             <PlayOptions />
         </div>
     );
