3cad5bc6879bf8161d490ab6ea0bc18b40b245db
slomg <74960133+slomg@users.noreply.github.com>
2025-06-05 15:11:33 +0300

    Add seeking overlay tests

--- frontend/src/components/play/SeekingOverlay.tsx
+++ frontend/src/components/play/SeekingOverlay.tsx
@@ -7,7 +7,10 @@ const SeekingOverlay = ({ onClick }: { onClick: () => void }) => {
             className="absolute inset-0 z-20 flex flex-col items-center justify-center rounded-sm"
             data-testid="seekingOverlay"
         >
-            <ArrowPathIcon className="w-10 animate-spin" />
+            <ArrowPathIcon
+                className="w-10 animate-spin"
+                data-testid="seekingSpinner"
+            />
             <div className="flex flex-col">
                 <span>Searching For a Match... </span>
                 <Button onClick={onClick} data-testid="cancelSeekButton">
