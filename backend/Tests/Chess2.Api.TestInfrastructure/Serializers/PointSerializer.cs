cdb20b3fd2be5eb1972ca64c52ec4c2420e109bb
slomg <74960133+slomg@users.noreply.github.com>
2025-06-06 10:07:34 +0300

    Point serializer

---
+++ backend/Tests/Chess2.Api.TestInfrastructure/Serializers/PointSerializer.cs
@@ -0,0 +1,45 @@
+﻿using Chess2.Api.GameLogic.Models;
+using Chess2.Api.TestInfrastructure.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+
+namespace Chess2.Api.TestInfrastructure.Serializers;
+
+public class PointSerializer : IXunitSerializer
+{
+    public object Deserialize(Type type, string serializedValue)
+    {
+        var point = serializedValue.Split("-");
+        var x = int.Parse(point[0]);
+        var y = int.Parse(point[1]);
+        return new Point(x, y);
+    }
+
+    public bool IsSerializable(Type type, object? value, out string failureReason)
+    {
+        if (type != typeof(Point))
+        {
+            failureReason = $"Type is not {nameof(Point)}";
+            return false;
+        }
+        if (value is not Point)
+        {
+            failureReason = $"Value is not of type {nameof(Point)}";
+            return false;
+        }
+        failureReason = "";
+        return true;
+    }
+
+    public string Serialize(object value)
+    {
+        if (value is not Point point)
+            throw new InvalidCastException("Could not cast ");
+        return $"{point.X}-{point.Y}";
+    }
+}
---
+++ backend/Tests/Chess2.Api.Unit/Tests/GameLogicTests/MovementBehaviourTests/SlideBehaviourTests.cs
@@ -0,0 +1,37 @@
+﻿using Chess2.Api.GameLogic;
+using Chess2.Api.GameLogic.Models;
+using Chess2.Api.GameLogic.MovementBehaviours;
+using Chess2.Api.TestInfrastructure.Fakes;
+using Chess2.Api.TestInfrastructure.Serializers;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+[assembly: RegisterXunitSerializer(typeof(PointSerializer), typeof(Point))]
+
+namespace Chess2.Api.Unit.Tests.GameLogicTests.MovementBehaviourTests;
+
+public class SlideBehaviourTests
+{
+    [Theory]
+    [ClassData(typeof(SlideBehaviourTestData))]
+    public void Test(Point from, Point offset)
+    {
+        var chessboard = new ChessBoard(new() [
+            [from] = new PieceFaker().Generate()
+        ])
+        var slideBehaviour = new SlideBehaviour(offset);
+    }
+}
+
+public class SlideBehaviourTestData : TheoryData<Point, Point>
+{
+    public SlideBehaviourTestData()
+    {
+        Add(new Point(1, 1), new Point(2, 2));
+    }
+}
