f64b2182995a7958ee08a293b9367253cd794077
slomg <74960133+slomg@users.noreply.github.com>
2025-06-09 10:56:14 +0300

    Improve AsUCI

--- backend/Chess2.Api/GameLogic/Models/Point.cs
+++ backend/Chess2.Api/GameLogic/Models/Point.cs
@@ -2,8 +2,6 @@
 
 public readonly record struct Point(int X, int Y)
 {
-    private const string Chars = "abcdefghijklmnopqrstuvwxyz";
-
     public static Point operator +(Point point1, Point point2) =>
         new(point1.X + point2.X, point1.Y + point2.Y);
 
@@ -12,6 +10,7 @@ public readonly record struct Point(int X, int Y)
 
     public string AsUCI()
     {
-        return $"{Chars[X]}{Y + 1}";
+        var rank = (char)('a' + X);
+        return $"{rank}{Y + 1}";
     }
 }
