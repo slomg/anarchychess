56deda0c60e835b156170151295f1225a518cd2f
slomg <74960133+slomg@users.noreply.github.com>
2025-06-11 16:14:01 +0300

    Invert color

---
+++ backend/Chess2.Api/GameLogic/Extensions/GameColorExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Chess2.Api.GameLogic.Models;
+
+namespace Chess2.Api.GameLogic.Extensions;
+
+public static class GameColorExtensions
+{
+    public static GameColor Invert(this GameColor color) => (GameColor)(1 ^ (int)color);
+}
---
+++ backend/Tests/Chess2.Api.Unit/Tests/GameLogicTests/GameExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using Chess2.Api.GameLogic.Extensions;
+using Chess2.Api.GameLogic.Models;
+using FluentAssertions;
+
+namespace Chess2.Api.Unit.Tests.GameLogicTests;
+
+public class GameExtensionsTests : BaseUnitTest
+{
+    [Theory]
+    [InlineData(GameColor.White, GameColor.Black)]
+    [InlineData(GameColor.Black, GameColor.White)]
+    public void Invert_should_invert_game_color(GameColor color, GameColor expected)
+    {
+        var result = color.Invert();
+        result.Should().Be(expected);
+    }
+}
