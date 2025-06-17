e048da426ccc1af5c5b22aecd7d65a306c90522c
slomg <74960133+slomg@users.noreply.github.com>
2025-06-17 18:02:08 +0300

    Add ConditionalBehaviour

---
+++ backend/Chess2.Api/GameLogic/MovementBehaviours/ConditionalBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using Chess2.Api.GameLogic.Models;
+
+namespace Chess2.Api.GameLogic.MovementBehaviours;
+
+public class ConditionalBehaviour(
+    Func<ChessBoard, AlgebraicPoint, Piece, bool> predicate,
+    IMovementBehaviour? trueBranch,
+    IMovementBehaviour? falseBranch
+) : IMovementBehaviour
+{
+    private readonly Func<ChessBoard, AlgebraicPoint, Piece, bool> _predicate = predicate;
+    private readonly IMovementBehaviour? _trueBranch = trueBranch;
+    private readonly IMovementBehaviour? _falseBranch = falseBranch;
+
+    public IEnumerable<AlgebraicPoint> Evaluate(
+        ChessBoard board,
+        AlgebraicPoint position,
+        Piece movingPiece
+    ) =>
+        _predicate(board, position, movingPiece)
+            ? _trueBranch?.Evaluate(board, position, movingPiece) ?? []
+            : _falseBranch?.Evaluate(board, position, movingPiece) ?? [];
+}
