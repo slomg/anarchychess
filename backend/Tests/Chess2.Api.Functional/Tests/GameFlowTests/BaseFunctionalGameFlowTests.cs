9e583a76376afb8043d0622a4e0355b92f19cca1
slomg <74960133+slomg@users.noreply.github.com>
2025-06-23 15:39:34 +0300

    Refactor matchmaking test helpers to base class

---
+++ backend/Tests/Chess2.Api.Functional/Tests/GameFlowTests/BaseFunctionalGameFlowTests.cs
@@ -0,0 +1,45 @@
+﻿using Chess2.Api.Game.Models;
+using Chess2.Api.TestInfrastructure;
+using FluentAssertions;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Chess2.Api.Functional.Tests.GameFlowTests;
+
+public abstract class BaseFunctionalGameFlowTests(Chess2WebApplicationFactory factory)
+    : BaseFunctionalTest(factory)
+{
+    protected static async Task AssertPlayersMatchAsync(
+        HubConnection conn1,
+        HubConnection conn2,
+        string methodName,
+        TimeControlSettings timeControl
+    )
+    {
+        await conn1.InvokeAsync(methodName, timeControl);
+        await conn2.InvokeAsync(methodName, timeControl);
+
+        await AssertMatchEstablishedAsync(conn1, conn2);
+    }
+
+    protected static async Task AssertMatchEstablishedAsync(
+        HubConnection conn1,
+        HubConnection conn2
+    )
+    {
+        var tcs1 = ListenForMatch(conn1);
+        var tcs2 = ListenForMatch(conn2);
+
+        var timeout = TimeSpan.FromSeconds(10);
+        var gameId1 = await tcs1.Task.WaitAsync(timeout, CT);
+        var gameId2 = await tcs2.Task.WaitAsync(timeout, CT);
+
+        gameId1.Should().NotBeNullOrEmpty().And.HaveLength(16).And.Be(gameId2);
+    }
+
+    protected static TaskCompletionSource<string> ListenForMatch(HubConnection conn)
+    {
+        var tcs = new TaskCompletionSource<string>();
+        conn.On<string>("MatchFoundAsync", gameId => tcs.TrySetResult(gameId));
+        return tcs;
+    }
+}
--- backend/Tests/Chess2.Api.Functional/Tests/GameFlowTests/MatchmakingHubTests.cs
+++ backend/Tests/Chess2.Api.Functional/Tests/GameFlowTests/MatchmakingHubTests.cs
@@ -7,9 +7,10 @@ using FluentAssertions;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
 
-namespace Chess2.Api.Functional.Tests;
+namespace Chess2.Api.Functional.Tests.GameFlowTests;
 
-public class MatchmakingHubTests(Chess2WebApplicationFactory factory) : BaseFunctionalTest(factory)
+public class MatchmakingHubTests(Chess2WebApplicationFactory factory)
+    : BaseFunctionalGameFlowTests(factory)
 {
     private const string HubPath = "/api/hub/matchmaking";
     private const string SeekCasualMethod = "SeekCasualAsync";
@@ -155,37 +156,5 @@ public class MatchmakingHubTests(Chess2WebApplicationFactory factory) : BaseFunc
         await guest2Conn.InvokeAsync(SeekCasualMethod, timeControl, CT);
 
         await AssertMatchEstablishedAsync(guest1ActiveConn, guest2Conn);
-    }
-
-    private static async Task AssertPlayersMatchAsync(
-        HubConnection conn1,
-        HubConnection conn2,
-        string methodName,
-        TimeControlSettings timeControl
-    )
-    {
-        await conn1.InvokeAsync(methodName, timeControl);
-        await conn2.InvokeAsync(methodName, timeControl);
-
-        await AssertMatchEstablishedAsync(conn1, conn2);
-    }
-
-    private static async Task AssertMatchEstablishedAsync(HubConnection conn1, HubConnection conn2)
-    {
-        var tcs1 = ListenForMatch(conn1);
-        var tcs2 = ListenForMatch(conn2);
-
-        var timeout = TimeSpan.FromSeconds(10);
-        var gameId1 = await tcs1.Task.WaitAsync(timeout, CT);
-        var gameId2 = await tcs2.Task.WaitAsync(timeout, CT);
-
-        gameId1.Should().NotBeNullOrEmpty().And.HaveLength(16).And.Be(gameId2);
-    }
-
-    private static TaskCompletionSource<string> ListenForMatch(HubConnection conn)
-    {
-        var tcs = new TaskCompletionSource<string>();
-        conn.On<string>("MatchFoundAsync", gameId => tcs.TrySetResult(gameId));
-        return tcs;
     }
 }
