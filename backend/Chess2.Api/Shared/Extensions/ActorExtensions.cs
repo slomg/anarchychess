ae647643c9b46cfbfbd23e9c941d325b50403f88
slomg <74960133+slomg@users.noreply.github.com>
2025-07-04 16:09:31 +0300

    Better ErrorOr handling with actors

--- backend/Chess2.Api/Shared/Extensions/ActorExtensions.cs
+++ backend/Chess2.Api/Shared/Extensions/ActorExtensions.cs
@@ -5,12 +5,50 @@ namespace Chess2.Api.Shared.Extensions;
 
 public static class ActorExtensions
 {
-    public static void ReplyWithErrorOr<T>(this IActorRef actor, T value) =>
-        actor.Tell(value.ToErrorOr());
+    public static void ReplyWithError(this IActorRef actor, Error value) =>
+        actor.Tell(value.OfType<object>());
 
-    public static void ReplyWithErrorOr<T>(this IActorRef actor, Error value) =>
-        actor.Tell(value.OfType<T>());
+    public static void ReplyWithError(this IActorRef actor, IEnumerable<Error> value) =>
+        actor.Tell(value.OfType<object>());
 
-    public static void ReplyWithErrorOr<T>(this IActorRef actor, IEnumerable<Error> value) =>
-        actor.Tell(value.OfType<T>());
+    /// <summary>
+    /// Sends a message to the actor and asynchronously waits for a response.
+    /// Expects the response to be either:
+    /// 1. An ErrorOr containing either errors or a value of type T,
+    /// 2. Or a raw value of type T.
+    /// </summary>
+    /// <typeparam name="T">The expected success value type</typeparam>
+    /// <returns>An ErrorOr<T> representing either the errors or the success value.</returns>
+    /// <exception cref="ArgumentException">the actor response type is not compatible with T</exception>
+    public static async Task<ErrorOr<T>> AskExpecting<T>(
+        this IActorRef actor,
+        object message,
+        CancellationToken token = default
+    )
+    {
+        var response = await actor.Ask<object>(message, token);
+        switch (response)
+        {
+            case ErrorOr<object> errorOr:
+                if (errorOr.IsError)
+                    return errorOr.Errors;
+
+                if (errorOr.Value is T typedValue)
+                    return typedValue;
+
+                throw new ArgumentException(
+                    $"Expected response of type {typeof(T)}, but got {errorOr.Value.GetType()}",
+                    nameof(message)
+                );
+
+            case T directValue:
+                return directValue;
+
+            default:
+                throw new ArgumentException(
+                    $"Expected response of type {typeof(T)}, but got {response.GetType()}",
+                    nameof(message)
+                );
+        }
+    }
 }
