0a234831e2380cc0bd659263b7758d1f87fc378d
slomg <74960133+slomg@users.noreply.github.com>
2025-07-14 14:05:46 +0300

    Delete unused ws types

--- frontend/src/types/tempModels.ts
+++ frontend/src/types/tempModels.ts
@@ -53,35 +53,6 @@ export interface FinishedGame {
     createdAt: number;
 }
 
-export const enum WSEventIn {
-    Notification = "notification",
-    GameStart = "game_start",
-}
-
-export const enum WSEventOut {
-    Move = "move",
-    Resign = "resign",
-}
-
-interface GameStart {
-    token: string;
-}
-
-export interface WSInEventMessageMap {
-    [WSEventIn.Notification]: null;
-    [WSEventIn.GameStart]: GameStart;
-}
-
-interface OutgoingMove {
-    origin: Point;
-    destination: Point;
-}
-
-export interface WSOutEventMessageMap {
-    [WSEventOut.Move]: OutgoingMove;
-    [WSEventOut.Resign]: null;
-}
-
 export interface Rating {
     elo: number;
     achievedAt: number;
