592553e6c1dddfbef272004ccb0af6207930d6dc
slomg <74960133+slomg@users.noreply.github.com>
2025-08-04 15:30:21 +0300

    Fix back navigation refetch

--- frontend/src/features/liveGame/hooks/useBackNavigationRefetch.ts
+++ frontend/src/features/liveGame/hooks/useBackNavigationRefetch.ts
@@ -1,23 +1,24 @@
 import { ChessboardStore } from "@/features/chessboard/stores/chessboardStore";
 import { usePathname } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { StoreApi } from "zustand";
 import { LiveChessStore } from "../stores/liveChessStore";
 import { refetchGame } from "../lib/gameStateProcessor";
+
+let lastPathname: string | null = null;
 
 export default function useBackNavigationRefetch(
     chessboardStore: StoreApi<ChessboardStore>,
     liveChessStore: StoreApi<LiveChessStore>,
 ) {
     const pathname = usePathname();
-    const lastPathname = useRef<string>(null);
 
     useEffect(() => {
-        if (pathname === lastPathname.current) {
-            lastPathname.current = null;
+        const resultData = liveChessStore.getState().resultData;
+        if (pathname === lastPathname && !resultData) {
             refetchGame(liveChessStore, chessboardStore);
         } else {
-            lastPathname.current = pathname;
+            lastPathname = pathname;
         }
-    }, [pathname, chessboardStore, liveChessStore]);
+    }, [pathname, liveChessStore, chessboardStore]);
 }
