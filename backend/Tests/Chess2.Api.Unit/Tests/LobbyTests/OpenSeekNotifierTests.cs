0b2eccecf394d51b3c819e7b1b93fc3a5515d1ac
slomg <74960133+slomg@users.noreply.github.com>
2025-08-13 17:08:35 +0300

    Add notify open seek for conn id test

--- backend/Tests/Chess2.Api.Unit/Tests/LobbyTests/OpenSeekNotifierTests.cs
+++ backend/Tests/Chess2.Api.Unit/Tests/LobbyTests/OpenSeekNotifierTests.cs
@@ -10,7 +10,29 @@ namespace Chess2.Api.Unit.Tests.LobbyTests;
 
 public class OpenSeekNotifierTests
 {
+    private readonly string _connId = "test conn";
     private readonly List<string> _userIds = ["user1", "user2", "user3"];
+    private readonly List<OpenSeek> _testSeeks =
+    [
+        new OpenSeek(
+            SeekKey: new SeekKey(
+                "user id",
+                new PoolKey(PoolType.Rated, new TimeControlSettings(600, 3))
+            ),
+            UserName: "username",
+            TimeControl: TimeControl.Rapid,
+            Rating: 123
+        ),
+        new OpenSeek(
+            SeekKey: new SeekKey(
+                "user id",
+                new PoolKey(PoolType.Casual, new TimeControlSettings(6, 35))
+            ),
+            UserName: "username",
+            TimeControl: TimeControl.Blitz,
+            Rating: null
+        ),
+    ];
 
     private readonly IHubContext<OpenSeekHub, IOpenSeekHubClient> _hubContextMock = Substitute.For<
         IHubContext<OpenSeekHub, IOpenSeekHubClient>
@@ -18,7 +40,9 @@ public class OpenSeekNotifierTests
     private readonly IHubClients<IOpenSeekHubClient> _clientsMock = Substitute.For<
         IHubClients<IOpenSeekHubClient>
     >();
-    private readonly IOpenSeekHubClient _clientProxyMock = Substitute.For<IOpenSeekHubClient>();
+    private readonly IOpenSeekHubClient _clientUsersProxyMock =
+        Substitute.For<IOpenSeekHubClient>();
+    private readonly IOpenSeekHubClient _clientConnProxyMock = Substitute.For<IOpenSeekHubClient>();
 
     private readonly OpenSeekNotifier _notifier;
 
@@ -26,7 +50,9 @@ public class OpenSeekNotifierTests
     {
         _clientsMock
             .Users(Arg.Is<IReadOnlyList<string>>(x => x.SequenceEqual(_userIds)))
-            .Returns(_clientProxyMock);
+            .Returns(_clientUsersProxyMock);
+        _clientsMock.Client(_connId).Returns(_clientConnProxyMock);
+
         _hubContextMock.Clients.Returns(_clientsMock);
         _notifier = new(_hubContextMock);
     }
@@ -34,31 +60,17 @@ public class OpenSeekNotifierTests
     [Fact]
     public async Task NotifyOpenSeekAsync_notifies_all_users_of_open_seeks()
     {
-        List<OpenSeek> seeks =
-        [
-            new OpenSeek(
-                SeekKey: new SeekKey(
-                    "user id",
-                    new PoolKey(PoolType.Rated, new TimeControlSettings(600, 3))
-                ),
-                UserName: "username",
-                TimeControl: TimeControl.Rapid,
-                Rating: 123
-            ),
-            new OpenSeek(
-                SeekKey: new SeekKey(
-                    "user id",
-                    new PoolKey(PoolType.Casual, new TimeControlSettings(6, 35))
-                ),
-                UserName: "username",
-                TimeControl: TimeControl.Blitz,
-                Rating: null
-            ),
-        ];
+        await _notifier.NotifyOpenSeekAsync(_userIds, _testSeeks);
 
-        await _notifier.NotifyOpenSeekAsync(_userIds, seeks);
+        await _clientUsersProxyMock.Received(1).NewOpenSeeksAsync(_testSeeks);
+    }
 
-        await _clientProxyMock.Received(1).NewOpenSeeksAsync(seeks);
+    [Fact]
+    public async Task NotifyOpenSeekAsync_with_a_connection_id_notifies_the_correct_client()
+    {
+        await _notifier.NotifyOpenSeekAsync(_connId, _testSeeks);
+
+        await _clientConnProxyMock.Received(1).NewOpenSeeksAsync(_testSeeks);
     }
 
     [Fact]
@@ -71,6 +83,6 @@ public class OpenSeekNotifierTests
 
         await _notifier.NotifyOpenSeekEndedAsync(_userIds, seekKey);
 
-        await _clientProxyMock.Received(1).OpenSeekEndedAsync(seekKey);
+        await _clientUsersProxyMock.Received(1).OpenSeekEndedAsync(seekKey);
     }
 }
