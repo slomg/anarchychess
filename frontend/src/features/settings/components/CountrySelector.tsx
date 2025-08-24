0cc23575178ab49853f4638fc821cdeae5cfee9f
slomg <74960133+slomg@users.noreply.github.com>
2025-08-24 14:36:34 +0300

    Add international

--- frontend/src/features/settings/components/CountrySelector.tsx
+++ frontend/src/features/settings/components/CountrySelector.tsx
@@ -1,9 +1,18 @@
+import { useAuthedUser } from "@/features/auth/hooks/useSessionUser";
+
 import TextField from "@/components/ui/TextField";
 import countries from "@public/data/countries.json";
 
 const CountrySelector = () => {
+    const user = useAuthedUser();
+    if (!user) return null;
+
     return (
-        <TextField label="Country" as="select">
+        <TextField
+            label="Country"
+            as="select"
+            defaultValue={user.countryCode ?? ""}
+        >
             {Object.entries(countries as Record<string, string>).map(
                 ([code, name]) => (
                     <option
