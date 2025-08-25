008bafa560ee7a84d264662ff7aa22490542f86c
slomg <74960133+slomg@users.noreply.github.com>
2025-08-25 14:44:26 +0300

    Update page.tsx

--- frontend/src/app/(public)/settings/page.tsx
+++ frontend/src/app/(public)/settings/page.tsx
@@ -7,7 +7,7 @@ import withAuthedUser from "@/features/auth/hocs/withAuthedUser";
 function Page() {
     return (
         <div className="flex w-full justify-center gap-5 p-5">
-            <Card className="w-full max-w-60 gap-0 p-0">
+            <Card className="w-full max-w-80 gap-0 p-0">
                 <button className="hover:bg-primary flex cursor-pointer items-center gap-2 p-5 text-lg transition">
                     <UserIcon className="h-8 w-8" />
                     Profile
