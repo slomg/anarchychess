89fd45819b1de9caa3d5b4fb0e86315e4a98221e
slomg <74960133+slomg@users.noreply.github.com>
2025-09-01 15:57:11 +0300

    Put games table in a card

--- frontend/src/features/profile/components/GamesTable.tsx
+++ frontend/src/features/profile/components/GamesTable.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Card from "@/components/ui/Card";
 import GameRow from "./GameRow";
 import { GameSummary, PublicUser } from "@/lib/apiClient";
 
@@ -11,48 +12,50 @@ const GamesTable = ({
     profileViewpoint: PublicUser;
 }) => {
     return (
-        <table className="h-min w-full table-auto overflow-x-auto">
-            <colgroup>
-                <col style={{ width: "50%" }} />
-                <col style={{ width: "30%" }} />
-                <col style={{ width: "20%" }} />
-            </colgroup>
-            <thead className="bg-card text-xl">
-                <tr data-testid="gamesTableHeader">
-                    <th scope="col" className="rounded-l-md p-4 text-start">
-                        Players
-                    </th>
-                    <th scope="col" className="p-3 text-start">
-                        Results
-                    </th>
-                    <th scope="col" className="rounded-r-md p-3 text-start">
-                        Date
-                    </th>
-                </tr>
-            </thead>
-            <tbody className="text-xl">
-                {games.length ? (
-                    games.map((game, i) => (
-                        <GameRow
-                            key={game.gameToken}
-                            game={game}
-                            profileViewpoint={profileViewpoint}
-                            index={i}
-                        />
-                    ))
-                ) : (
-                    <tr className="bg-gray-400/5">
-                        <td
-                            colSpan={3}
-                            className="p-5 text-center"
-                            data-testid="noGamesRow"
-                        >
-                            This user hasn&#39;t played any games yet
-                        </td>
+        <Card className="overflow-x-auto p-0.5">
+            <table className="h-min table-auto overflow-x-auto">
+                <colgroup>
+                    <col style={{ width: "50%" }} />
+                    <col style={{ width: "30%" }} />
+                    <col style={{ width: "20%" }} />
+                </colgroup>
+                <thead className="bg-card text-xl">
+                    <tr data-testid="gamesTableHeader">
+                        <th scope="col" className="rounded-l-md p-4 text-start">
+                            Players
+                        </th>
+                        <th scope="col" className="p-3 text-start">
+                            Results
+                        </th>
+                        <th scope="col" className="rounded-r-md p-3 text-start">
+                            Date
+                        </th>
                     </tr>
-                )}
-            </tbody>
-        </table>
+                </thead>
+                <tbody className="text-xl">
+                    {games.length ? (
+                        games.map((game, i) => (
+                            <GameRow
+                                key={game.gameToken}
+                                game={game}
+                                profileViewpoint={profileViewpoint}
+                                index={i}
+                            />
+                        ))
+                    ) : (
+                        <tr className="bg-gray-400/5">
+                            <td
+                                colSpan={3}
+                                className="p-5 text-center"
+                                data-testid="noGamesRow"
+                            >
+                                This user hasn&#39;t played any games yet
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </Card>
     );
 };
 export default GamesTable;
