8576cde660b8fc6fba6624ad8c3b7cb7f51b09a6
slomg <74960133+slomg@users.noreply.github.com>
2025-09-01 12:10:32 +0300

    Change UserId type in MinimalProfile

--- backend/Chess2.Api/Profile/DTOs/MinimalProfile.cs
+++ backend/Chess2.Api/Profile/DTOs/MinimalProfile.cs
@@ -1,11 +1,10 @@
-﻿using System.Text.Json.Serialization;
-using Chess2.Api.Profile.Entities;
-using Chess2.Api.Profile.Models;
+﻿using Chess2.Api.Profile.Entities;
+using System.Text.Json.Serialization;
 
 namespace Chess2.Api.Profile.DTOs;
 
 [method: JsonConstructor]
-public record MinimalProfile(UserId UserId, string UserName)
+public record MinimalProfile(string UserId, string UserName)
 {
     public MinimalProfile(AuthedUser user)
         : this(user.Id, user.UserName ?? "Unknown") { }
