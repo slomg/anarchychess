6c7405af2a54d80281ec4a1a5a15fd223ea0fd8f
slomg <74960133+slomg@users.noreply.github.com>
2025-09-06 11:49:51 +0300

    Add blocked user navigation

--- backend/Chess2.Api/Social/Entities/BlockedUser.cs
+++ backend/Chess2.Api/Social/Entities/BlockedUser.cs
@@ -1,8 +1,17 @@
-﻿namespace Chess2.Api.Social.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Chess2.Api.Profile.Entities;
+using Microsoft.EntityFrameworkCore;
 
+namespace Chess2.Api.Social.Entities;
+
+[Index(nameof(UserId), nameof(BlockedUserId), IsUnique = true)]
 public class BlockedUser
 {
     public int Id { get; set; }
     public required string UserId { get; set; }
+
     public required string BlockedUserId { get; set; }
+
+    [ForeignKey(nameof(BlockedUserId))]
+    public required AuthedUser Blocked { get; set; }
 }
