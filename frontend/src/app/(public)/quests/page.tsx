abd79142928e6574d950df01150dc6b73e3cf2e6
slomg <74960133+slomg@users.noreply.github.com>
2025-09-13 13:56:52 +0300

    Add logged out quest card

--- frontend/src/app/(public)/quests/page.tsx
+++ frontend/src/app/(public)/quests/page.tsx
@@ -1,6 +1,7 @@
 import Card from "@/components/ui/Card";
 import WithOptionalAuthedUser from "@/features/auth/components/WithOptionalAuthedUser";
 import DailyQuestCard from "@/features/quests/components/DailyQuestCard";
+import DailyQuestCardLoggedOut from "@/features/quests/components/DailyQuestCardLoggedOut";
 import { getDailyQuest, Quest } from "@/lib/apiClient";
 import dataOrThrow from "@/lib/apiClient/dataOrThrow";
 
@@ -19,7 +20,12 @@ export default async function QuestsPage() {
 
                 return (
                     <div className="flex w-full flex-col items-center gap-6 p-5">
-                        {dailyQuest && <DailyQuestCard quest={dailyQuest} />}
+                        {dailyQuest ? (
+                            <DailyQuestCard quest={dailyQuest} />
+                        ) : (
+                            <DailyQuestCardLoggedOut />
+                        )}
+
                         <Card className="w-full max-w-3xl">
                             <h2 className="text-3xl">Leaderboard</h2>
                         </Card>
---
+++ frontend/src/features/quests/components/DailyQuestCardLoggedOut.tsx
@@ -0,0 +1,35 @@
+import Button from "@/components/ui/Button";
+import Card from "@/components/ui/Card";
+import constants from "@/lib/constants";
+import Link from "next/link";
+
+const DailyQuestCardLoggedOut = () => {
+    return (
+        <Card className="h-fit w-full max-w-3xl gap-6 p-6">
+            <h1 className="text-4xl" data-testid="dailyQuestTitle">
+                Daily Quest
+            </h1>
+
+            <div className="flex flex-col gap-4">
+                <p
+                    className="text-text/70 text-lg"
+                    data-testid="dailyQuestMessage"
+                >
+                    Register to start completing daily quests, earn streaks, and
+                    track your progress!
+                </p>
+
+                <Button>
+                    <Link
+                        href={constants.PATHS.REGISTER}
+                        className="flex h-full w-full items-center justify-center"
+                        data-testid="dailyQuestRegisterLink"
+                    >
+                        Register
+                    </Link>
+                </Button>
+            </div>
+        </Card>
+    );
+};
+export default DailyQuestCardLoggedOut;
