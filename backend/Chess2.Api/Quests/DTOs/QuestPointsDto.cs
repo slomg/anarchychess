e7cba41c940dfde5166db903cda9ffb215bccf19
slomg <74960133+slomg@users.noreply.github.com>
2025-09-14 14:59:44 +0300

    Add display name

--- backend/Chess2.Api/Quests/DTOs/QuestPointsDto.cs
+++ backend/Chess2.Api/Quests/DTOs/QuestPointsDto.cs
@@ -1,5 +1,7 @@
 ﻿using Chess2.Api.Profile.DTOs;
+using System.ComponentModel;
 
 namespace Chess2.Api.Quests.DTOs;
 
+[DisplayName("UserQuestPoints")]
 public record QuestPointsDto(MinimalProfile Profile, int QuestPoints);
