bebcad05862304ddccb9c141fa3136ce2afb5fe6
slomg <74960133+slomg@users.noreply.github.com>
2025-09-14 11:03:27 +0300

    Add quest leaderboard service

---
+++ backend/Chess2.Api/Quests/Services/QuestLeaderboardService.cs
@@ -0,0 +1,49 @@
+﻿using Chess2.Api.Pagination.Models;
+using Chess2.Api.Profile.DTOs;
+using Chess2.Api.Profile.Entities;
+using Chess2.Api.Quests.DTOs;
+using Chess2.Api.Quests.Repositories;
+using Chess2.Api.Shared.Models;
+
+namespace Chess2.Api.Quests.Services;
+
+public interface IQuestLeaderboardService
+{
+    Task<PagedResult<QuestPointsDto>> GetPaginatedLeaderboardAsync(
+        PaginationQuery pagination,
+        CancellationToken token = default
+    );
+    Task<int> GetRankingAsync(AuthedUser user, CancellationToken token = default);
+}
+
+public class QuestLeaderboardService(IQuestLeaderboardRepository questLeaderboardRepository)
+    : IQuestLeaderboardService
+{
+    private readonly IQuestLeaderboardRepository _questLeaderboardRepository =
+        questLeaderboardRepository;
+
+    public async Task<PagedResult<QuestPointsDto>> GetPaginatedLeaderboardAsync(
+        PaginationQuery pagination,
+        CancellationToken token = default
+    )
+    {
+        var users = await _questLeaderboardRepository.GetPaginatedLeaderboardAsync(
+            pagination,
+            token
+        );
+        var totalCount = await _questLeaderboardRepository.GetTotalCountAsync(token);
+
+        return new(
+            Items: users.Select(user => new QuestPointsDto(
+                new MinimalProfile(user),
+                user.QuestPoints
+            )),
+            TotalCount: totalCount,
+            Page: pagination.Page,
+            PageSize: pagination.PageSize
+        );
+    }
+
+    public Task<int> GetRankingAsync(AuthedUser user, CancellationToken token = default) =>
+        _questLeaderboardRepository.GetRankingAsync(user, token);
+}
---
+++ backend/Tests/Chess2.Api.Integration/Tests/QuestTests/QuestLeaderboardServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Chess2.Api.Pagination.Models;
+using Chess2.Api.Profile.Entities;
+using Chess2.Api.Quests.Services;
+using Chess2.Api.TestInfrastructure;
+using Chess2.Api.TestInfrastructure.Fakes;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chess2.Api.Integration.Tests.QuestTests;
+
+public class QuestLeaderboardServiceTests : BaseIntegrationTest
+{
+    private readonly IQuestLeaderboardService _questLeaderboardService;
+
+    public QuestLeaderboardServiceTests(Chess2WebApplicationFactory factory)
+        : base(factory)
+    {
+        _questLeaderboardService =
+            Scope.ServiceProvider.GetRequiredService<IQuestLeaderboardService>();
+    }
+
+    [Fact]
+    public async Task GetPaginatedLeaderboardAsync_applies_pagination()
+    {
+        List<AuthedUser> users =
+        [
+            new AuthedUserFaker().RuleFor(x => x.QuestPoints, 4),
+            new AuthedUserFaker().RuleFor(x => x.QuestPoints, 3),
+            new AuthedUserFaker().RuleFor(x => x.QuestPoints, 2),
+            new AuthedUserFaker().RuleFor(x => x.QuestPoints, 1),
+        ];
+        await DbContext.AddRangeAsync(users, CT);
+        await DbContext.SaveChangesAsync(CT);
+
+        PaginationQuery pagination = new(Page: 0, PageSize: 3);
+
+        var result = await _questLeaderboardService.GetPaginatedLeaderboardAsync(pagination, CT);
+
+        result.Items.Should().BeEquivalentTo(users[..3]);
+        result.TotalCount.Should().Be(users.Count);
+        result.Page.Should().Be(pagination.Page);
+        result.PageSize.Should().Be(pagination.PageSize);
+    }
+}
