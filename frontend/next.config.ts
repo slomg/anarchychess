a47ef699ed719ea2c2b41e3accc9f2fb2ced13fb
slomg <74960133+slomg@users.noreply.github.com>
2025-09-29 11:45:38 +0300

    Disable dev indicator

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: { remotePatterns: [new URL("https://localhost:7266/**")] },
+    devIndicators: false,
 };
 
 export default nextConfig;
