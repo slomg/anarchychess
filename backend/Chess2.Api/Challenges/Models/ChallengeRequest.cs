8a7dc5e973d44edf39471ce4230b3746503e5d56
slomg <74960133+slomg@users.noreply.github.com>
2025-09-30 17:48:05 +0300

    Rename IncomingChallenge to ChallengeRequest

--- backend/Chess2.Api/Challenges/Models/ChallengeRequest.cs
+++ backend/Chess2.Api/Challenges/Models/ChallengeRequest.cs
@@ -1,13 +1,14 @@
-﻿using Chess2.Api.GameSnapshot.Models;
+﻿using Chess2.Api.Matchmaking.Models;
 using Chess2.Api.Profile.DTOs;
 
 namespace Chess2.Api.Challenges.Models;
 
 [GenerateSerializer]
 [Alias("Chess2.Api.Challenges.Models.IncomingChallenge")]
-public record IncomingChallenge(
+public record ChallengeRequest(
     ChallengeId ChallengeId,
     MinimalProfile Requester,
-    TimeControlSettings TimeControl,
+    MinimalProfile Recipient,
+    PoolKey Pool,
     DateTime ExpiresAt
 );
---
+++ backend/Tests/Chess2.Api.TestInfrastructure/Fakes/ChallengeRequestFaker.cs
@@ -0,0 +1,15 @@
+﻿using Chess2.Api.Challenges.Models;
+
+namespace Chess2.Api.TestInfrastructure.Fakes;
+
+public class ChallengeRequestFaker : RecordFaker<ChallengeRequest>
+{
+    public ChallengeRequestFaker()
+    {
+        StrictMode(true);
+        RuleFor(x => x.ChallengeId, f => (ChallengeId)f.Random.Guid().ToString()[..16]);
+        RuleFor(x => x.Requester, f => new MinimalProfileFaker().Generate());
+        RuleFor(x => x.Pool, f => new PoolKeyFaker().Generate());
+        RuleFor(x => x.ExpiresAt, f => f.Date.Future());
+    }
+}
