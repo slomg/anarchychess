df41b427f44591b0eb6dd95e8af7be5acddc5556
slomg <74960133+slomg@users.noreply.github.com>
2025-09-30 17:50:09 +0300

    Fix ChallengeRequestFaker

--- backend/Tests/Chess2.Api.TestInfrastructure/Fakes/ChallengeRequestFaker.cs
+++ backend/Tests/Chess2.Api.TestInfrastructure/Fakes/ChallengeRequestFaker.cs
@@ -9,6 +9,7 @@ public class ChallengeRequestFaker : RecordFaker<ChallengeRequest>
         StrictMode(true);
         RuleFor(x => x.ChallengeId, f => (ChallengeId)f.Random.Guid().ToString()[..16]);
         RuleFor(x => x.Requester, f => new MinimalProfileFaker().Generate());
+        RuleFor(x => x.Recipient, f => new MinimalProfileFaker().Generate());
         RuleFor(x => x.Pool, f => new PoolKeyFaker().Generate());
         RuleFor(x => x.ExpiresAt, f => f.Date.Future());
     }
