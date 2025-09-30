8ca2eec9d7885ba962cb89bc23db0e16cd257626
slomg <74960133+slomg@users.noreply.github.com>
2025-09-30 17:48:18 +0300

    Create PoolKeyFaker.cs

---
+++ backend/Tests/Chess2.Api.TestInfrastructure/Fakes/PoolKeyFaker.cs
@@ -0,0 +1,20 @@
+﻿using Chess2.Api.GameSnapshot.Models;
+using Chess2.Api.Matchmaking.Models;
+
+namespace Chess2.Api.TestInfrastructure.Fakes;
+
+public class PoolKeyFaker : RecordFaker<PoolKey>
+{
+    public PoolKeyFaker()
+    {
+        StrictMode(true);
+        RuleFor(x => x.PoolType, f => f.PickRandom<PoolType>());
+        RuleFor(
+            x => x.TimeControl,
+            f => new TimeControlSettings(
+                BaseSeconds: f.Random.Number(100, 1000),
+                IncrementSeconds: f.Random.Number(10, 100)
+            )
+        );
+    }
+}
