ffcff82b3b578172c2d78aff7f054c5c188ac50c
slomg <74960133+slomg@users.noreply.github.com>
2025-10-01 16:13:55 +0300

    Move challenge actions to a controller

---
+++ backend/Chess2.Api/Challenges/Controllers/ChallengeController.cs
@@ -0,0 +1,65 @@
+﻿using Chess2.Api.Auth.Services;
+using Chess2.Api.Challenges.Grains;
+using Chess2.Api.Challenges.Models;
+using Chess2.Api.Infrastructure.Extensions;
+using Chess2.Api.Matchmaking.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chess2.Api.Challenges.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+[Authorize]
+public class ChallengeController(IGrainFactory grains, IAuthService authService) : Controller
+{
+    private readonly IGrainFactory _grains = grains;
+    private readonly IAuthService _authService = authService;
+
+    [HttpPut("{recipientId}", Name = nameof(CreateChallenge))]
+    [ProducesResponseType<ChallengeRequest>(StatusCodes.Status204NoContent)]
+    public async Task<ActionResult<ChallengeRequest>> CreateChallenge(
+        string recipientId,
+        PoolKey pool
+    )
+    {
+        var userIdResult = _authService.GetUserId(User);
+        if (userIdResult.IsError)
+            return userIdResult.Errors.ToActionResult();
+
+        var id = Guid.NewGuid().ToString()[..16];
+        var challengeGrain = _grains.GetGrain<IChallengeGrain>(id);
+        var result = await challengeGrain.CreateAsync(
+            requester: userIdResult.Value,
+            recipient: recipientId,
+            pool
+        );
+        return result.Match(Ok, errors => errors.ToActionResult());
+    }
+
+    [HttpDelete("{challengeId}", Name = nameof(CancelChallenge))]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<ActionResult> CancelChallenge(string challengeId)
+    {
+        var userIdResult = _authService.GetUserId(User);
+        if (userIdResult.IsError)
+            return userIdResult.Errors.ToActionResult();
+
+        var challengeGrain = _grains.GetGrain<IChallengeGrain>(challengeId);
+        var result = await challengeGrain.CancelAsync(userIdResult.Value);
+        return result.Match(value => NoContent(), errors => errors.ToActionResult());
+    }
+
+    [HttpPost("{challengeId}/accept")]
+    [ProducesResponseType<string>(StatusCodes.Status200OK)]
+    public async Task<ActionResult<string>> AcceptChallenge(string challengeId)
+    {
+        var userIdResult = _authService.GetUserId(User);
+        if (userIdResult.IsError)
+            return userIdResult.Errors.ToActionResult();
+
+        var challengeGrain = _grains.GetGrain<IChallengeGrain>(challengeId);
+        var result = await challengeGrain.AcceptAsync(userIdResult.Value);
+        return result.Match(Ok, errors => errors.ToActionResult());
+    }
+}
--- backend/Chess2.Api/Challenges/SignalR/ChallengeHub.cs
+++ backend/Chess2.Api/Challenges/SignalR/ChallengeHub.cs
@@ -3,9 +3,6 @@ using Chess2.Api.Challenges.Models;
 using Chess2.Api.Challenges.Services;
 using Chess2.Api.Infrastructure;
 using Chess2.Api.Infrastructure.SignalR;
-using Chess2.Api.Matchmaking.Models;
-using Chess2.Api.Profile.Models;
-using ErrorOr;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Chess2.Api.Challenges.SignalR;
@@ -23,35 +20,6 @@ public class ChallengeHub(IGrainFactory grains, IChallengeNotifier challengeNoti
 {
     private readonly IGrainFactory _grains = grains;
     private readonly IChallengeNotifier _challengeNotifier = challengeNotifier;
-
-    public async Task CreateChallengeAsync(UserId recipientId, PoolKey pool)
-    {
-        if (!TryGetUserId(out var requesterId))
-        {
-            await HandleErrors(Error.Unauthorized());
-            return;
-        }
-
-        var id = Guid.NewGuid().ToString()[..16];
-        var challengeGrain = _grains.GetGrain<IChallengeGrain>(id);
-        var result = await challengeGrain.CreateAsync(requesterId, recipientId, pool);
-        if (result.IsError)
-            await HandleErrors(result.Errors);
-    }
-
-    public async Task CancelChallengeAsync(ChallengeId challengeId)
-    {
-        if (!TryGetUserId(out var userId))
-        {
-            await HandleErrors(Error.Unauthorized());
-            return;
-        }
-
-        var challengeGrain = _grains.GetGrain<IChallengeGrain>(challengeId);
-        var result = await challengeGrain.CancelAsync(cancelledBy: userId);
-        if (result.IsError)
-            await HandleErrors(result.Errors);
-    }
 
     public override async Task OnConnectedAsync()
     {
