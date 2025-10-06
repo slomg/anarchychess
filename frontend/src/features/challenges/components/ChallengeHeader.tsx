af8e45c438995bcf491ce3a9021027ce731d04fd
slomg <74960133+slomg@users.noreply.github.com>
2025-10-06 14:37:18 +0300

    Add challenge header tests

--- frontend/src/features/challenges/components/ChallengeHeader.tsx
+++ frontend/src/features/challenges/components/ChallengeHeader.tsx
@@ -1,19 +1,30 @@
 import Card from "@/components/ui/Card";
 import TimeControlIcon from "@/features/lobby/Components/TimeControlIcon";
-import { ChallengeRequest, PoolType } from "@/lib/apiClient";
+import { PoolType } from "@/lib/apiClient";
+import useChallengeStore from "../hooks/useChallengeStore";
 
-const ChallengeHeader = ({ challenge }: { challenge: ChallengeRequest }) => {
+const ChallengeHeader = () => {
+    const challenge = useChallengeStore((x) => x.challenge);
     const isRated = challenge.pool.poolType == PoolType.RATED;
 
     return (
         <Card className="items-center">
-            <h1 className="text-4xl font-bold">Challenge</h1>
+            <h1
+                data-testid="challengeHeaderTitle"
+                className="text-4xl font-bold"
+            >
+                Challenge
+            </h1>
             <div className="flex items-center gap-2">
                 <TimeControlIcon
                     timeControl={challenge.timeControl}
                     className="h-8 w-8"
+                    data-testid="challengeHeaderTimeControlIcon"
                 />
-                <p className="text-2xl font-semibold">
+                <p
+                    data-testid="challengeHeaderTimeControl"
+                    className="text-2xl font-semibold"
+                >
                     {challenge.pool.timeControl.baseSeconds / 60}+
                     {challenge.pool.timeControl.incrementSeconds}{" "}
                     {isRated ? "Rated" : "Casual"}
---
+++ frontend/src/features/challenges/components/__tests__/ChallengeHeader.test.tsx
@@ -0,0 +1,71 @@
+import { ChallengeRequest, PoolType } from "@/lib/apiClient";
+import { StoreApi } from "zustand";
+import {
+    ChallengeStore,
+    createChallengeStore,
+} from "../../stores/challengeStore";
+import { createFakeChallengeRequest } from "@/lib/testUtils/fakers/challengeRequestFaker";
+import { render, screen } from "@testing-library/react";
+import ChallengeStoreContext from "../../contexts/challengeContext";
+import ChallengeHeader from "../ChallengeHeader";
+
+describe("ChallengeHeader", () => {
+    let challengeMock: ChallengeRequest;
+    let challengeStore: StoreApi<ChallengeStore>;
+
+    beforeEach(() => {
+        challengeMock = createFakeChallengeRequest();
+        challengeStore = createChallengeStore({ challenge: challengeMock });
+    });
+
+    it("should render the challenge title", () => {
+        render(
+            <ChallengeStoreContext.Provider value={challengeStore}>
+                <ChallengeHeader />
+            </ChallengeStoreContext.Provider>,
+        );
+
+        expect(screen.getByTestId("challengeHeaderTitle")).toHaveTextContent(
+            "Challenge",
+        );
+    });
+
+    it("should display the correct time control and increment", () => {
+        render(
+            <ChallengeStoreContext.Provider value={challengeStore}>
+                <ChallengeHeader />
+            </ChallengeStoreContext.Provider>,
+        );
+
+        const expectedText = `${challengeMock.pool.timeControl.baseSeconds / 60}+${challengeMock.pool.timeControl.incrementSeconds} Rated`;
+        expect(
+            screen.getByTestId("challengeHeaderTimeControl"),
+        ).toHaveTextContent(expectedText);
+    });
+
+    it("should show 'Rated' when pool type is rated", () => {
+        render(
+            <ChallengeStoreContext.Provider value={challengeStore}>
+                <ChallengeHeader />
+            </ChallengeStoreContext.Provider>,
+        );
+
+        expect(
+            screen.getByTestId("challengeHeaderTimeControl"),
+        ).toHaveTextContent("Rated");
+    });
+
+    it("should show 'Casual' when pool type is casual", () => {
+        challengeMock.pool.poolType = PoolType.CASUAL;
+
+        render(
+            <ChallengeStoreContext.Provider value={challengeStore}>
+                <ChallengeHeader />
+            </ChallengeStoreContext.Provider>,
+        );
+
+        expect(
+            screen.getByTestId("challengeHeaderTimeControl"),
+        ).toHaveTextContent("Casual");
+    });
+});
