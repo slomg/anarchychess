dcbdc5af6e81face7c1dd8a2e3fefbde6b28b875
slomg <74960133+slomg@users.noreply.github.com>
2025-10-09 11:44:46 +0300

    Add WithSession to PlayPage

--- frontend/src/app/(public)/play/page.tsx
+++ frontend/src/app/(public)/play/page.tsx
@@ -1,19 +1,21 @@
+import WithSession from "@/features/auth/components/WithSession";
 import StaticChessboardWithSidebar from "@/features/chessboard/components/StaticChessboardWithSidebar";
 import OpenSeekDirectory from "@/features/play/components/OpenSeekDirectory";
 import PlayOptions from "@/features/play/components/PlayOptions";
 
 export const metadata = { title: "Play - Chess 2" };
 
-const PlayPage = () => {
+export default function PlayPage() {
     return (
-        <StaticChessboardWithSidebar
-            aside={
-                <aside className="grid h-full w-full min-w-xs grid-rows-[auto_1fr] gap-3 lg:max-w-sm">
-                    <PlayOptions />
-                    <OpenSeekDirectory />
-                </aside>
-            }
-        />
+        <WithSession>
+            <StaticChessboardWithSidebar
+                aside={
+                    <aside className="grid h-full w-full min-w-xs grid-rows-[auto_1fr] gap-3 lg:max-w-sm">
+                        <PlayOptions />
+                        <OpenSeekDirectory />
+                    </aside>
+                }
+            />
+        </WithSession>
     );
-};
-export default PlayPage;
+}
