95a016dee16cea8de39001343f80b34af7600e7f
slomg <74960133+slomg@users.noreply.github.com>
2025-10-13 16:11:16 +0300

    Add bottom home page section

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 
+import StaticChessboard from "@/features/chessboard/components/StaticChessboard";
 import Knook from "@public/assets/pieces-svg/knook-white.svg";
 import Button from "@/components/ui/Button";
+import Pog from "@public/assets/pog.png";
 import Card from "@/components/ui/Card";
 
 export const metadata = { title: "Home - Chess 2" };
 
 const HomePage = async () => {
     return (
-        <div className="grid flex-1 grid-rows-[auto_1fr]">
+        <div className="flex flex-1 flex-col gap-10">
             <section
                 className="bg-checkerboard relative flex flex-col items-center justify-center gap-3
                     overflow-hidden bg-[#151515] bg-[length:10rem_10rem] bg-center p-3 py-30
@@ -23,7 +25,7 @@ const HomePage = async () => {
                     <Image
                         src={Knook}
                         alt="knook"
-                        width={720}
+                        width={700}
                         className="absolute -bottom-100 -left-60 hidden max-w-none rotate-15 xl:block"
                     />
                 </div>
@@ -72,12 +74,44 @@ const HomePage = async () => {
                     </Card>
                 </div>
             </section>
-            {/* 
-            <div className="bg-background h-full w-full">
-                <h1 className="text-5xl">
-                    Discover the Madness of Anarchy Chess
-                </h1>
-            </div> */}
+
+            <section
+                className="grid w-full grid-rows-[min-content_auto] justify-center gap-20 p-5
+                    lg:grid-cols-[minmax(auto,max-content)_auto]"
+            >
+                <div className="flex w-full flex-col gap-3 lg:max-w-lg">
+                    <h1 className="text-5xl">
+                        Discover the Madness of Anarchy Chess
+                    </h1>
+
+                    <p className="text-text/80 text-xl">
+                        Don&apos;t worry, no one knows what&apos;s going on
+                    </p>
+                    <p className="text-text/80 flex gap-2 text-sm">
+                        look at this cool game on the side though
+                        <Image src={Pog} alt="pog" width={20} height={20} />
+                    </p>
+
+                    <Button className="bg-secondary mt-5 max-w-sm text-3xl text-black">
+                        PLAY NOW
+                    </Button>
+                </div>
+
+                <StaticChessboard
+                    breakpoints={[
+                        {
+                            maxScreenSize: 767,
+                            paddingOffset: { width: 45, height: 0 },
+                        },
+                        {
+                            maxScreenSize: 1024,
+                            paddingOffset: { width: 145, height: 0 }, // p-5 + p-5 + sidebar
+                        },
+                    ]}
+                    defaultOffset={{ width: 500, height: 0, maxSize: 400 }}
+                    className="justify-self-center"
+                />
+            </section>
         </div>
     );
 };
