97b46bbaee3e5cc4f3aca74d88eb8c28b5e72e2e
slomg <74960133+slomg@users.noreply.github.com>
2025-10-23 13:55:01 +0300

    Fix home hero gap

--- frontend/src/features/home/components/HomeHero.tsx
+++ frontend/src/features/home/components/HomeHero.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 const HomeHero = () => {
     return (
         <header
-            className="bg-checkerboard relative flex flex-col items-center justify-center gap-3
+            className="bg-checkerboard relative flex flex-col items-center justify-center gap-10
                 overflow-hidden bg-[#151515] bg-[length:10rem_10rem] bg-center p-3 py-10
                 md:gap-10 md:px-10 xl:flex-row xl:items-start xl:gap-20 xl:py-30"
         >
