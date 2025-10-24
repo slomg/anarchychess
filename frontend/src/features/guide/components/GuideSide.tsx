ea603585d99c82191343e78bcc0fefc0b6fa3826
slomg <74960133+slomg@users.noreply.github.com>
2025-10-24 12:10:41 +0300

    Update GuideSide.tsx

--- frontend/src/features/guide/components/GuideSide.tsx
+++ frontend/src/features/guide/components/GuideSide.tsx
@@ -9,22 +9,24 @@ const GuideSide = ({
     rulesGuideHref: string;
 }) => {
     return (
-        <Card className="sticky top-5 hidden max-h-[calc(100vh-2.5rem)] w-full max-w-30 gap-0 p-0 lg:flex">
-            <Link
-                href={piecesGuideHref}
-                className="hover:bg-primary flex flex-1 cursor-pointer items-center justify-center text-2xl
-                    font-semibold transition"
-            >
-                Pieces
-            </Link>
-            <hr className="text-secondary/50" />
-            <Link
-                href={rulesGuideHref}
-                className="hover:bg-primary flex flex-1 cursor-pointer items-center justify-center text-2xl
-                    font-semibold transition"
-            >
-                Rules
-            </Link>
+        <Card className="hidden max-w-30 flex-1 gap-0 p-0 lg:flex">
+            <div className="sticky top-0 flex h-screen flex-col">
+                <Link
+                    href={piecesGuideHref}
+                    className="hover:bg-primary flex flex-1 cursor-pointer items-center justify-center text-2xl
+                        font-semibold transition"
+                >
+                    Pieces
+                </Link>
+                <hr className="text-secondary/50" />
+                <Link
+                    href={rulesGuideHref}
+                    className="hover:bg-primary flex flex-1 cursor-pointer items-center justify-center text-2xl
+                        font-semibold transition"
+                >
+                    Rules
+                </Link>
+            </div>
         </Card>
     );
 };
