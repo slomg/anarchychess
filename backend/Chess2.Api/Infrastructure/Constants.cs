ab35491dcf59665e46c65fa24cd899b37caab92d
slomg <74960133+slomg@users.noreply.github.com>
2025-11-05 17:15:23 +0200

    Remove unused tournament state  constant

--- backend/Chess2.Api/Infrastructure/Constants.cs
+++ backend/Chess2.Api/Infrastructure/Constants.cs
@@ -9,7 +9,6 @@ public static class StorageNames
 {
     public const string PlayerSessionState = "PlayerSessionState";
     public const string MatchmakingState = "MatchmakingState";
-    public const string TournamentState = "TournamentState";
     public const string ChallengeState = "ChallengeState";
     public const string RematchState = "RematchState";
     public const string QuestState = "QuestState";
