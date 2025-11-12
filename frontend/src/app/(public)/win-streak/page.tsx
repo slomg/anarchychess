269a35faf7b4e41024903969ee513ce4e8b20902
slomg <74960133+slomg@users.noreply.github.com>
2025-11-12 14:55:54 +0200

    Add title to win streak page

--- frontend/src/app/(public)/win-streak/page.tsx
+++ frontend/src/app/(public)/win-streak/page.tsx
@@ -2,6 +2,8 @@ import WinStreakObjective from "@/features/winStreak/components/WinStreakObjecti
 import WinStreakHeader from "@/features/winStreak/components/WinStreakHeader";
 import WinStreakRules from "@/features/winStreak/components/WinStreakRules";
 
+export const metadata = { title: "Win Streak Challenge - Chess 2" };
+
 export default async function WinStreakPage() {
     return (
         <main className="mx-auto flex max-w-6xl flex-1 flex-col items-center gap-3 p-5">
