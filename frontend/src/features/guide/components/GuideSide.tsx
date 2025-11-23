30b55ed6e92fbb1d6a924493a711abf7f0a9301b
slomg <74960133+slomg@users.noreply.github.com>
2025-11-23 16:47:10 +0200

    Add rounded corners to GuideSide navigation links

--- frontend/src/features/guide/components/GuideSide.tsx
+++ frontend/src/features/guide/components/GuideSide.tsx
@@ -12,16 +12,16 @@ const GuideSide = ({
         <Card className="sticky top-5 hidden max-h-[calc(100vh-2.5rem)] max-w-30 flex-1 gap-0 p-0 lg:flex">
             <Link
                 href={piecesGuideHref}
-                className="hover:bg-primary flex flex-1 cursor-pointer items-center justify-center text-2xl
-                    font-semibold transition"
+                className="hover:bg-primary flex flex-1 cursor-pointer items-center justify-center
+                    rounded-t-md text-2xl font-semibold transition"
             >
                 Pieces
             </Link>
             <hr className="text-secondary/50" />
             <Link
                 href={rulesGuideHref}
-                className="hover:bg-primary flex flex-1 cursor-pointer items-center justify-center text-2xl
-                    font-semibold transition"
+                className="hover:bg-primary flex flex-1 cursor-pointer items-center justify-center
+                    rounded-b-md text-2xl font-semibold transition"
             >
                 Rules
             </Link>
