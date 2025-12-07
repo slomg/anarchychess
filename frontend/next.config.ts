3cdcbe2b5d826d84f157508963b1732e6ca69681
slomg <74960133+slomg@users.noreply.github.com>
2025-12-07 18:29:35 +0200

    Add anarchychess domain to images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: { remotePatterns: [new URL("https://localhost:7266/**")] },
+    images: {
+        remotePatterns: [
+            new URL("https://localhost:7266/**"),
+            new URL("https://api.anarchychess.org/**"),
+        ],
+    },
     devIndicators: false,
 };
 
