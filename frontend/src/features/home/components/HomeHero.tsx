d341d7d0ee41ae58d83e911367c194e979818670
slomg <74960133+slomg@users.noreply.github.com>
2025-12-08 20:07:20 +0200

    Move cta buttons in home hero down

--- frontend/src/features/home/components/HomeHero.tsx
+++ frontend/src/features/home/components/HomeHero.tsx
@@ -9,16 +9,21 @@ import Link from "next/link";
 const HomeHero = () => {
     return (
         <header
-            className="bg-checkerboard relative flex flex-col items-center justify-center gap-10
-                overflow-hidden bg-center p-3 py-10 md:gap-10 md:px-10 xl:flex-row
-                xl:items-start xl:gap-20 xl:py-35"
+            className="bg-checkerboard relative flex flex-col items-center
+                justify-center gap-10 overflow-hidden bg-center p-3 py-10
+                md:gap-10 md:px-10 xl:flex-row xl:items-start xl:gap-20
+                xl:py-35"
         >
             <div
-                className="relative flex h-full flex-col text-center text-nowrap xl:text-start"
+                className="relative flex h-full flex-col text-center text-nowrap
+                    xl:text-start"
                 data-testid="homeHeroBanner"
             >
                 <p className="text-4xl sm:text-5xl">Welcome To</p>
-                <h1 className="text-7xl min-[375px]:text-8xl sm:text-9xl xl:ml-10">
+                <h1
+                    className="text-7xl min-[375px]:text-8xl sm:text-9xl
+                        xl:ml-10"
+                >
                     CHESS
                 </h1>
                 <h2 className="text-3xl xl:text-end">The Anarchy Update</h2>
@@ -27,13 +32,20 @@ const HomeHero = () => {
                     src={Knook}
                     alt="knook"
                     width={700}
-                    className="absolute -bottom-107 -left-70 hidden max-w-none rotate-15 xl:block"
+                    className="absolute -bottom-107 -left-70 hidden max-w-none
+                        rotate-15 xl:block"
                     priority
                 />
             </div>
 
-            <div className="grid h-min max-w-180 grid-cols-2 items-stretch gap-10">
-                <Card className="border-accent relative col-span-2 gap-5 border-4 p-2">
+            <div
+                className="grid h-min max-w-180 grid-cols-2 items-stretch
+                    gap-10"
+            >
+                <Card
+                    className="border-accent relative col-span-2 justify-between
+                        gap-5 border-4 p-2"
+                >
                     <div className="flex flex-col gap-2">
                         <h1 className="text-4xl text-balance sm:text-5xl">
                             PLAY ANARCHY CHESS!
@@ -50,7 +62,10 @@ const HomeHero = () => {
                     </Link>
                 </Card>
 
-                <Card className="bg-background border-primary border-4 p-2 sm:gap-10">
+                <Card
+                    className="bg-background border-primary justify-between
+                        border-4 p-2 sm:gap-10"
+                >
                     <div className="flex flex-col gap-2">
                         <h1 className="text-3xl">DAILY QUEST</h1>
                         <p className="text-sm text-balance md:text-base">
@@ -59,13 +74,19 @@ const HomeHero = () => {
                     </div>
 
                     <Link href={constants.PATHS.QUESTS} className="w-full">
-                        <Button className="mt-auto w-full text-[clamp(0.8rem,4vw,2rem)] sm:text-3xl">
+                        <Button
+                            className="mt-auto w-full
+                                text-[clamp(0.8rem,4vw,2rem)] sm:text-3xl"
+                        >
                             PLAY QUEST
                         </Button>
                     </Link>
                 </Card>
 
-                <Card className="bg-background border-primary border-4 p-2 sm:gap-10">
+                <Card
+                    className="bg-background border-primary justify-between
+                        border-4 p-2 sm:gap-10"
+                >
                     <div className="flex flex-col gap-2">
                         <h1 className="text-3xl">NEW RULES</h1>
                         <p className="text-sm text-balance md:text-base">
@@ -74,7 +95,10 @@ const HomeHero = () => {
                     </div>
 
                     <Link href={constants.PATHS.GUIDE} className="w-full">
-                        <Button className="mt-auto w-full text-[clamp(0.8rem,4vw,2rem)] sm:text-3xl">
+                        <Button
+                            className="mt-auto w-full
+                                text-[clamp(0.8rem,4vw,2rem)] sm:text-3xl"
+                        >
                             VIEW GUIDE
                         </Button>
                     </Link>
