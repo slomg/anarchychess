2f7a78d7dfcf77bc5c1d0057022177c8cc397b73
slomg <74960133+slomg@users.noreply.github.com>
2025-12-08 16:25:04 +0200

    Fix quest season resetter period assertions

--- backend/Tests/AnarchyChess.Api.Integration/Tests/QuestTests/QuestSeasonResetterGrainTests.cs
+++ backend/Tests/AnarchyChess.Api.Integration/Tests/QuestTests/QuestSeasonResetterGrainTests.cs
@@ -44,7 +44,7 @@ public class QuestSeasonResetterGrainTests : BaseOrleansIntegrationTest
                 Silo.GetGrainId(grain),
                 QuestSeasonResetterGrain.ReminderName,
                 dueTime,
-                TimeSpan.FromDays(10)
+                TimeSpan.FromMinutes(10)
             )
         );
     }
@@ -97,7 +97,7 @@ public class QuestSeasonResetterGrainTests : BaseOrleansIntegrationTest
                 Silo.GetGrainId(grain),
                 QuestSeasonResetterGrain.ReminderName,
                 It.IsAny<TimeSpan>(),
-                TimeSpan.FromDays(10)
+                TimeSpan.FromMinutes(10)
             )
         );
     }
