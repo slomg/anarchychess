7bb5cf9d86086770372ed0a9e3ad4e257eefdc94
slomg <74960133+slomg@users.noreply.github.com>
2026-01-06 12:21:49 +0200

    Add title to system pages

--- frontend/src/app/(system)/logout/page.tsx
+++ frontend/src/app/(system)/logout/page.tsx
@@ -2,9 +2,14 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { Metadata } from "next";
 
 import { logout } from "@/lib/apiClient";
 import constants from "@/lib/constants";
+
+export const metadata: Metadata = {
+    title: "Log Out - Anarchy Chess",
+};
 
 const LogoutPage = () => {
     const router = useRouter();
--- frontend/src/app/(system)/refresh/page.tsx
+++ frontend/src/app/(system)/refresh/page.tsx
@@ -1,2 +1,9 @@
+import { Metadata } from "next";
+
 import RefreshRedirect from "@/features/auth/components/RefreshRedirect";
+
+export const metadata: Metadata = {
+    title: "Please Wait...",
+};
+
 export default RefreshRedirect;
