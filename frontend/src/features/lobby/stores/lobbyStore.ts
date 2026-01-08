4ca35adefa20e82433f49bcf8e2387adf96bc182
slomg <74960133+slomg@users.noreply.github.com>
2026-01-08 16:48:42 +0200

    Remove devtools from lobby store

--- frontend/src/features/lobby/stores/lobbyStore.ts
+++ frontend/src/features/lobby/stores/lobbyStore.ts
@@ -1,6 +1,5 @@
 import { createWithEqualityFn } from "zustand/traditional";
 import { immer } from "zustand/middleware/immer";
-import { devtools } from "zustand/middleware";
 import { shallow } from "zustand/shallow";
 import { enableMapSet } from "immer";
 
@@ -23,48 +22,46 @@ interface LobbyStore {
 
 enableMapSet();
 const useLobbyStore = createWithEqualityFn<LobbyStore>()(
-    devtools(
-        immer((set) => ({
-            seeks: new Set(),
-            requestedOpenSeek: false,
-            ongoingGames: new Map(),
+    immer((set) => ({
+        seeks: new Set(),
+        requestedOpenSeek: false,
+        ongoingGames: new Map(),
 
-            clearSeeks() {
-                set((state) => {
-                    state.seeks.clear();
-                });
-            },
-            addSeek(pool) {
-                set((state) => {
-                    state.seeks.add(pool);
-                });
-            },
-            removeSeek(pool) {
-                set((state) => {
-                    state.seeks.delete(pool);
-                });
-            },
+        clearSeeks() {
+            set((state) => {
+                state.seeks.clear();
+            });
+        },
+        addSeek(pool) {
+            set((state) => {
+                state.seeks.add(pool);
+            });
+        },
+        removeSeek(pool) {
+            set((state) => {
+                state.seeks.delete(pool);
+            });
+        },
 
-            setRequestedOpenSeek(isRequesting) {
-                set((state) => {
-                    state.requestedOpenSeek = isRequesting;
-                });
-            },
+        setRequestedOpenSeek(isRequesting) {
+            set((state) => {
+                state.requestedOpenSeek = isRequesting;
+            });
+        },
 
-            addOngoingGames(games) {
-                set((state) => {
-                    for (const game of games) {
-                        state.ongoingGames.set(game.gameToken, game);
-                    }
-                });
-            },
-            removeOngoingGame(gameToken) {
-                set((state) => {
-                    state.ongoingGames.delete(gameToken);
-                });
-            },
-        })),
-    ),
+        addOngoingGames(games) {
+            set((state) => {
+                for (const game of games) {
+                    state.ongoingGames.set(game.gameToken, game);
+                }
+            });
+        },
+        removeOngoingGame(gameToken) {
+            set((state) => {
+                state.ongoingGames.delete(gameToken);
+            });
+        },
+    })),
     shallow,
 );
 export default useLobbyStore;
