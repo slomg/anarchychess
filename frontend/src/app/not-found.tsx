c7f7dc8dfd6bada53612e5a82d89e0494062a80f
slomg <74960133+slomg@users.noreply.github.com>
2026-01-10 15:22:17 +0200

    Update not-found.tsx

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Button from "@/components/ui/Button";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-    title: "Not Found",
+    title: "Not Found - Anarchy Chess",
 };
 
 export default function NotFound() {
