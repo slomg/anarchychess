7a90af239e938db8a0feff9c70958e72a8173efc
slomg <74960133+slomg@users.noreply.github.com>
2026-01-14 11:48:51 +0200

    Move game grain interface and state into separate files

---
+++ backend/AnarchyChess.Api/Game/Grains/IGameGrain.cs
@@ -0,0 +1,53 @@
+﻿using AnarchyChess.Api.Game.Models;
+using AnarchyChess.Api.GameLogic.Models;
+using AnarchyChess.Api.GameSnapshot.Models;
+using AnarchyChess.Api.Matchmaking.Models;
+using AnarchyChess.Api.Profile.Models;
+using AnarchyChess.Api.Shared.Models;
+using ErrorOr;
+
+namespace AnarchyChess.Api.Game.Grains;
+
+[Alias("AnarchyChess.Api.Game.Grains.IGameGrain")]
+public interface IGameGrain : IGrainWithStringKey
+{
+    [Alias("StartGameAsync")]
+    Task StartGameAsync(
+        GamePlayer whitePlayer,
+        GamePlayer blackPlayer,
+        PoolKey pool,
+        GameSource gameSource,
+        CancellationToken token = default
+    );
+
+    [Alias("SyncRevisionAsync")]
+    Task<ErrorOr<Success>> SyncRevisionAsync(
+        ConnectionId connectionId,
+        CancellationToken token = default
+    );
+
+    [Alias("GetStateAsync")]
+    Task<ErrorOr<GameState>> GetStateAsync();
+
+    [Alias("GetPlayersAsync")]
+    Task<ErrorOr<PlayerRoster>> GetPlayersAsync();
+
+    [Alias("GetMovesAsync")]
+    Task<ErrorOr<IReadOnlyList<Move>>> GetMovesAsync();
+
+    [Alias("RequestGameEndAsync")]
+    Task<ErrorOr<Success>> RequestGameEndAsync(UserId byUserId, CancellationToken token = default);
+
+    [Alias("RequestDrawAsync")]
+    Task<ErrorOr<Success>> RequestDrawAsync(UserId byUserId, CancellationToken token = default);
+
+    [Alias("DeclineDrawAsync")]
+    Task<ErrorOr<Success>> DeclineDrawAsync(UserId byUserId, CancellationToken token = default);
+
+    [Alias("MovePieceAsync")]
+    Task<ErrorOr<Success>> MovePieceAsync(
+        UserId byUserId,
+        MoveKey key,
+        CancellationToken token = default
+    );
+}
---
+++ backend/AnarchyChess.Api/Game/Models/GameGrainState.cs
@@ -0,0 +1,48 @@
+﻿using AnarchyChess.Api.Game.Services;
+using AnarchyChess.Api.GameSnapshot.Models;
+using AnarchyChess.Api.Matchmaking.Models;
+
+namespace AnarchyChess.Api.Game.Models;
+
+[GenerateSerializer]
+[Alias("AnarchyChess.Api.Game.Models.GameData")]
+public class GameData
+{
+    [Id(1)]
+    public required PlayerRoster Players { get; init; }
+
+    [Id(2)]
+    public required GameSource GameSource { get; init; }
+
+    [Id(3)]
+    public required PoolKey Pool { get; init; }
+
+    [Id(4)]
+    public required string InitialFen { get; init; }
+
+    [Id(5)]
+    public List<MoveSnapshot> MoveSnapshots { get; init; } = [];
+
+    [Id(6)]
+    public required GameCoreState Core { get; init; }
+
+    [Id(7)]
+    public required DrawRequestState DrawRequest { get; init; }
+
+    [Id(8)]
+    public required GameClockState ClockState { get; init; }
+
+    [Id(9)]
+    public required GameNotifierState NotifierState { get; init; }
+
+    [Id(10)]
+    public GameResultData? Result { get; set; }
+}
+
+[GenerateSerializer]
+[Alias("AnarchyChess.Api.Game.Models.GameGrainState")]
+public class GameGrainState
+{
+    [Id(0)]
+    public GameData? CurrentGame { get; set; }
+}
