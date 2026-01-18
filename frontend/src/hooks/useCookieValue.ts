1b140a819920ce6d4724380e2ae3680364aea7a3
slomg <74960133+slomg@users.noreply.github.com>
2026-01-18 17:08:28 +0200

    Return undefined instead of null by default in useCookieValue

--- frontend/src/hooks/useCookieValue.ts
+++ frontend/src/hooks/useCookieValue.ts
@@ -4,8 +4,8 @@ import Cookies from "js-cookie";
 export default function useCookieValue<T>(
     cookieName: string,
     defaultValue: T,
-): T | null {
-    const [value, setValue] = useState<T | null>(null);
+): T | undefined {
+    const [value, setValue] = useState<T | undefined>();
 
     const setValueFromCookie = useEffectEvent(() => {
         const cookieValue = Cookies.get(cookieName);
