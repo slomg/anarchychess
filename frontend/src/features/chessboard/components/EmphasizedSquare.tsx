5b7bc18ae2f0589ed670da13c2b9d1ac6c0629fc
slomg <74960133+slomg@users.noreply.github.com>
2026-01-19 12:03:07 +0200

    Add emphasized square

---
+++ frontend/src/features/chessboard/components/EmphasizedSquare.tsx
@@ -0,0 +1,27 @@
+import { useChessboardStore } from "../hooks/useChessboard";
+import { pointToStr } from "@/features/point/pointUtils";
+import { LogicalPoint } from "@/features/point/types";
+import ChessSquare from "./ChessSquare";
+
+const EmphasizedSquaresRenderer = () => {
+    const emphasizedSquares = useChessboardStore(
+        (x) => x.getViewedPositionLegalMoves().emphasizedSquares,
+    );
+
+    return emphasizedSquares.map((point) => (
+        <EmphasizedSquare position={point} key={pointToStr(point)} />
+    ));
+};
+export default EmphasizedSquaresRenderer;
+
+const EmphasizedSquare = ({ position }: { position: LogicalPoint }) => {
+    return (
+        <ChessSquare
+            data-testid="emphasizedSquare"
+            className="before:animate-freakout animate-fade-in z-20
+                before:absolute before:inset-0 before:border-4
+                before:border-red-500"
+            position={position}
+        />
+    );
+};
---
+++ frontend/src/features/chessboard/components/__tests__/EmphasizedSquare.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import { StoreApi } from "zustand";
+
+import {
+    ChessboardStore,
+    createChessboardStore,
+} from "../../stores/chessboardStore";
+
+import ChessboardStoreContext from "../../contexts/chessboardStoreContext";
+import EmphasizedSquaresRenderer from "../EmphasizedSquare";
+import { logicalPoint } from "@/features/point/pointUtils";
+import LegalMoves from "../../lib/legalMoves";
+
+describe("EmphasizedSquaresRenderer", () => {
+    let store: StoreApi<ChessboardStore>;
+
+    beforeEach(() => {
+        store = createChessboardStore();
+    });
+
+    it("should render all emphasized squares at the correct positions", () => {
+        const squares = [
+            logicalPoint({ x: 1, y: 2 }),
+            logicalPoint({ x: 3, y: 4 }),
+        ];
+
+        const legalMoves = new LegalMoves([], false, squares);
+        store.setState({
+            getViewedPositionLegalMoves: () => legalMoves,
+        });
+
+        render(
+            <ChessboardStoreContext.Provider value={store}>
+                <EmphasizedSquaresRenderer />
+            </ChessboardStoreContext.Provider>,
+        );
+
+        const renderedSquares = screen.getAllByTestId("emphasizedSquare");
+        expect(renderedSquares).toHaveLength(squares.length);
+
+        squares.forEach((sq) => {
+            const square = renderedSquares.find(
+                (el) => el.getAttribute("data-position") === `${sq.x},${sq.y}`,
+            );
+            expect(square).toBeInTheDocument();
+        });
+    });
+});
