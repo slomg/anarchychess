17ce1946310e00fbcd74591f4ada41082e6669c7
slomg <74960133+slomg@users.noreply.github.com>
2026-02-02 17:02:50 +0200

    Move bot to backend folder

--- backend/EntropyBot/BitMove.cs
+++ backend/EntropyBot/BitMove.cs
@@ -2,11 +2,12 @@
 
 struct BitMove
 {
-    public int From;
-    public int To;
+    public byte From;
+    public byte To;
     public BitPiece Piece;
 
     public UInt128 Captures;
     public BitPiece? PromotesTo;
+    public BitMovePriority ForcedMovePriority;
     public BitMoveFlag Flags;
 }
---
+++ backend/EntropyBot/BitMovePriority.cs
@@ -0,0 +1,8 @@
+﻿namespace EntropyBot;
+
+public enum BitMovePriority : byte
+{
+    None = 0,
+    UnderagePawn = 1,
+    EnPassant = 2,
+}
