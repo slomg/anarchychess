05edb0b30bcef9bc6a225320d1338fa36ca0a627
slomg <74960133+slomg@users.noreply.github.com>
2026-02-02 16:38:51 +0200

    Add bit piece

---
+++ EntropyBot/BitPiece.cs
@@ -0,0 +1,30 @@
+﻿namespace EntropyBot;
+
+public enum BitPiece
+{
+    WhiteKing,
+    WhiteQueen,
+    WhitePawn,
+    WhiteRook,
+    WhiteBishop,
+    WhiteHorsey,
+    WhiteKnook,
+    WhiteAntiqueen,
+    WhiteUnderagePawn,
+    WhiteSterilePawn,
+    WhiteChecker,
+
+    BlackKing,
+    BlackQueen,
+    BlackPawn,
+    BlackRook,
+    BlackBishop,
+    BlackHorsey,
+    BlackKnook,
+    BlackAntiqueen,
+    BlackUnderagePawn,
+    BlackSterilePawn,
+    BlackChecker,
+
+    TraitorRook,
+}
