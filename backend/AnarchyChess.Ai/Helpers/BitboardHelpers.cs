cd1e4509dd08d91da88cf5c0d427d4720d8b99e0
slomg <74960133+slomg@users.noreply.github.com>
2026-02-05 16:42:39 +0200

    Fix bitboard helpers

--- backend/AnarchyChess.Ai/Helpers/BitboardHelpers.cs
+++ backend/AnarchyChess.Ai/Helpers/BitboardHelpers.cs
@@ -1,11 +1,16 @@
 ﻿using System.Numerics;
 
-namespace AnarchyChess.Ai.Helpers;
+namespace AnarchyChess.Ai;
 
-public class BitboardHelpers
+public static class BitboardHelpers
 {
     public static int BitScanForward(ref UInt128 bitboard)
     {
+        if (bitboard == 0)
+        {
+            throw new InvalidOperationException("Cannot scan forward on an empty bitboard");
+        }
+
         ulong low = (ulong)bitboard;
         if (low != 0)
         {
@@ -13,22 +18,12 @@ public class BitboardHelpers
             bitboard &= bitboard - 1;
             return index;
         }
-
-        ulong high = (ulong)(bitboard >> 64);
-        if (high != 0)
+        else
         {
-            int index = BitOperations.TrailingZeroCount(high) + 64;
+            ulong high = (ulong)(bitboard >> 64);
+            int index = 64 + BitOperations.TrailingZeroCount(high);
             bitboard &= bitboard - 1;
             return index;
         }
-
-        throw new InvalidOperationException("BitScanForward on empty bitboard");
-    }
-
-    public static int PopCount(UInt128 bitboard)
-    {
-        ulong low = (ulong)bitboard;
-        ulong high = (ulong)(bitboard >> 64);
-        return BitOperations.PopCount(low) + BitOperations.PopCount(high);
     }
 }
