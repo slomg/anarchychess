e0f2d283a0a59a943d362edc69157f882770b57d
slomg <74960133+slomg@users.noreply.github.com>
2026-02-05 14:42:13 +0200

    Add bitboard helper tests

---
+++ backend/Tests/AnarchyChess.Ai.Tests/Tests/BitboardHelpersTests.cs
@@ -0,0 +1,46 @@
+﻿using AwesomeAssertions;
+
+namespace AnarchyChess.Ai.Tests.Tests;
+
+public class BitboardHelpersTests
+{
+    [Fact]
+    public void BitScanForward_returns_the_index_of_the_least_significant_bit()
+    {
+        UInt128 bitboard = 0b10010;
+        int index = BitboardHelpers.BitScanForward(ref bitboard);
+
+        index.Should().Be(1);
+        bitboard.Should().Be(0b10000);
+    }
+
+    [Fact]
+    public void BitScanForward_handles_a_single_bit_set()
+    {
+        UInt128 bitboard = 0b10000000;
+        int index = BitboardHelpers.BitScanForward(ref bitboard);
+
+        index.Should().Be(7);
+        bitboard.Should().Be(0);
+    }
+
+    [Fact]
+    public void BitScanForward_handles_high_bit_set()
+    {
+        UInt128 bitboard = UInt128.One << 67;
+        int index = BitboardHelpers.BitScanForward(ref bitboard);
+
+        index.Should().Be(67);
+        bitboard.Should().Be(0);
+    }
+
+    [Fact]
+    public void BitScanForward_throws_for_an_empty_bitboard()
+    {
+        UInt128 bitboard = 0;
+
+        Action act = () => BitboardHelpers.BitScanForward(ref bitboard);
+
+        act.Should().Throw<InvalidOperationException>();
+    }
+}
