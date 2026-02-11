24f29b8b30d3d434fca54c20671762976b472101
slomg <74960133+slomg@users.noreply.github.com>
2026-02-11 18:57:00 +0200

    Create TTEntry.cs

---
+++ backend/AnarchyChess.Ai/Models/TTEntry.cs
@@ -0,0 +1,17 @@
+﻿namespace AnarchyChess.Ai.Models;
+
+public enum TTNodeType
+{
+    Exact,
+    Alpha,
+    Beta,
+}
+
+public struct TTEntry
+{
+    public UInt128 Key;
+    public float Score;
+    public int Depth;
+    public TTNodeType NodeType;
+    public BitMove? BestMove;
+}
