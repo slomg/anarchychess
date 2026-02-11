59dd9473ededd2529864580cb4144bfcb7f4e4d5
slomg <74960133+slomg@users.noreply.github.com>
2026-02-11 16:21:37 +0200

    Add move ordering

--- backend/AnarchyChess.Ai/AiEngine.cs
+++ backend/AnarchyChess.Ai/AiEngine.cs
@@ -1,4 +1,6 @@
-﻿using AnarchyChess.Ai.Models;
+﻿using AnarchyChess.Ai.Helpers;
+using AnarchyChess.Ai.Models;
+using AnarchyChess.EngineShared;
 
 namespace AnarchyChess.Ai;
 
@@ -11,6 +13,7 @@ public class AiEngine(IBitMovesGenerator? moveGenerator = null, IEvaluator? eval
     : IAiEngine
 {
     private const int MaxMoves = 256;
+    private readonly int PieceCount = Enum.GetValues<PieceType>().Length;
 
     private readonly IBitMovesGenerator _moveGenerator = moveGenerator ?? new BitMovesGenerator();
     private readonly IEvaluator _evaluator = evaluator ?? new Evaluator();
@@ -19,7 +22,10 @@ public class AiEngine(IBitMovesGenerator? moveGenerator = null, IEvaluator? eval
     {
         Span<BitMove> moves = stackalloc BitMove[MaxMoves];
         int moveCount = 0;
-        _moveGenerator.Generate(board, moves, ref moveCount);
+        Span<int> moveCountByPlace = stackalloc int[PieceCount];
+
+        _moveGenerator.Generate(board, moves, ref moveCount, moveCountByPlace);
+        OrderMove(board, moves, moveCount);
 
         BitMove? bestMove = null;
         float bestScore = float.NegativeInfinity;
@@ -33,7 +39,8 @@ public class AiEngine(IBitMovesGenerator? moveGenerator = null, IEvaluator? eval
                 board,
                 depth - 1,
                 alpha: float.NegativeInfinity,
-                beta: float.PositiveInfinity
+                beta: float.PositiveInfinity,
+                prevMoveCountByPiece: moveCountByPlace
             );
 
             board.UndoMove(undo);
@@ -48,31 +55,42 @@ public class AiEngine(IBitMovesGenerator? moveGenerator = null, IEvaluator? eval
         return bestMove;
     }
 
-    private float Negamax(BitBoard board, int depth, float alpha, float beta)
+    private float Negamax(
+        BitBoard board,
+        int depth,
+        float alpha,
+        float beta,
+        Span<int> prevMoveCountByPiece
+    )
     {
         if (depth <= 0)
         {
-            return _evaluator.EvaluateBoard(board);
+            return _evaluator.EvaluateBoard(board, moveCountByPiece: prevMoveCountByPiece);
         }
 
         float best = float.NegativeInfinity;
 
         Span<BitMove> moves = stackalloc BitMove[MaxMoves];
         int moveCount = 0;
-        _moveGenerator.Generate(board, moves, ref moveCount);
+        Span<int> moveCountByPiece = stackalloc int[PieceCount];
+
+        _moveGenerator.Generate(board, moves, ref moveCount, moveCountByPiece);
+        OrderMove(board, moves, moveCount);
 
         for (int i = 0; i < moveCount; i++)
         {
-            UInt128 whiteBefore = board.WhitePieces;
-
             BitMove move = moves[i];
             MoveUndoState undo = board.MakeMove(move);
 
-            float score = -Negamax(board, depth - 1, alpha: -beta, beta: -alpha);
+            float score = -Negamax(
+                board,
+                depth - 1,
+                alpha: -beta,
+                beta: -alpha,
+                prevMoveCountByPiece: moveCountByPiece
+            );
 
             board.UndoMove(undo);
-
-            UInt128 whiteAfter = board.WhitePieces;
 
             best = Math.Max(best, score);
             alpha = Math.Max(alpha, score);
@@ -84,5 +102,46 @@ public class AiEngine(IBitMovesGenerator? moveGenerator = null, IEvaluator? eval
         }
 
         return best;
+    }
+
+    private static void OrderMove(BitBoard board, Span<BitMove> moves, int moveCount)
+    {
+        int write = 0;
+        for (int i = 0; i < moveCount; i++)
+        {
+            if (ScoreMove(moves[i], board) > 0)
+            {
+                if (i != write)
+                {
+                    (moves[write], moves[i]) = (moves[i], moves[write]);
+                }
+                write++;
+            }
+        }
+    }
+
+    private static int ScoreMove(BitMove move, BitBoard board)
+    {
+        if (move.CapturesMask != 0)
+        {
+            UInt128 captureMask = move.CapturesMask;
+            int score = 0;
+            while (captureMask != 0)
+            {
+                byte captureSquare = (byte)BitboardHelpers.BitScanForward(ref captureMask);
+                if (board.TryGetPieceAt(captureSquare, out var capturePiece))
+                {
+                    score += (int)Evaluator.GetPieceValue(capturePiece.Value.Type);
+                }
+            }
+            return 10_000 + score;
+        }
+
+        if (move.PromotesTo is not null)
+        {
+            return 9_000 + (int)Evaluator.GetPieceValue(move.PromotesTo.Value);
+        }
+
+        return 0;
     }
 }
