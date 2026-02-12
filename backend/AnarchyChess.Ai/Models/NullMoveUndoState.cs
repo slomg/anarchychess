7110cace7c062a6ee8e3fe3bddd7c12ffc970091
slomg <74960133+slomg@users.noreply.github.com>
2026-02-12 19:42:36 +0200

    Create NullMoveUndoState.cs

---
+++ backend/AnarchyChess.Ai/Models/NullMoveUndoState.cs
@@ -0,0 +1,8 @@
+﻿namespace AnarchyChess.Ai.Models;
+
+public struct NullMoveUndoState
+{
+    public required bool PrevIsWhiteToMove;
+    public required UInt128 PrevEnPassantSquaresMask;
+    public required byte PrevEnPassantPawnSquare;
+}
