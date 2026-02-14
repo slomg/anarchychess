33cb0a5e1fa4cb88fe397c2eb9347c299c2c2dad
slomg <74960133+slomg@users.noreply.github.com>
2026-02-14 19:06:05 +0200

    Add pawn structure evaluation

---
+++ backend/AnarchyChess.Ai/Evaluation/PawnStructureEvaluator.cs
@@ -0,0 +1,158 @@
+﻿using AnarchyChess.Ai.Helpers;
+using AnarchyChess.Ai.Models;
+using AnarchyChess.EngineShared;
+
+namespace AnarchyChess.Ai.Evaluation;
+
+public static class PawnStructureEvaluator
+{
+    private static readonly UInt128[] FileMasks = CreateFileMasks();
+
+    public const int DoubledPenalty = 12;
+    public const int IsolatedPenalty = 18;
+    public const int BackwardsPenalty = 10;
+    public const int PassedBonus = 35;
+
+    private static UInt128[] CreateFileMasks()
+    {
+        UInt128[] masks = new UInt128[10];
+        for (int file = 0; file < 10; file++)
+        {
+            UInt128 mask = 0;
+            for (int rank = 0; rank < 10; rank++)
+            {
+                mask |= UInt128.One << (rank * 10 + file);
+            }
+            masks[file] = mask;
+        }
+        return masks;
+    }
+
+    public static int Evaluate(BitBoard board)
+    {
+        int whiteScore = 0;
+        int blackScore = 0;
+
+        UInt128 whitePawns =
+            board.BitboardFor(PieceType.Pawn, BitPieceColor.White)
+            | board.BitboardFor(PieceType.UnderagePawn, BitPieceColor.White);
+        UInt128 blackPawns =
+            board.BitboardFor(PieceType.Pawn, BitPieceColor.Black)
+            | board.BitboardFor(PieceType.UnderagePawn, BitPieceColor.Black);
+
+        for (int file = 0; file < 10; file++)
+        {
+            whiteScore -= CountDoubled(whitePawns, file) * DoubledPenalty;
+            blackScore -= CountDoubled(blackPawns, file) * DoubledPenalty;
+        }
+
+        whiteScore -= CountIsolated(whitePawns) * IsolatedPenalty;
+        blackScore -= CountIsolated(blackPawns) * IsolatedPenalty;
+
+        whiteScore -= CountWhiteBackwards(whitePawns, enemyPawns: blackPawns) * BackwardsPenalty;
+        blackScore -= CountBlackBackwards(blackPawns, enemyPawns: whitePawns) * BackwardsPenalty;
+
+        whiteScore += CountWhitePassed(whitePawns, enemyPawns: blackPawns) * PassedBonus;
+        blackScore += CountBlackPassed(blackPawns, enemyPawns: whitePawns) * PassedBonus;
+
+        return board.IsWhiteToMove ? whiteScore - blackScore : blackScore - whiteScore;
+    }
+
+    private static int CountDoubled(UInt128 pawns, int file) =>
+        Math.Max(0, BitboardHelpers.CountBits(pawns & FileMasks[file]) - 1);
+
+    private static int CountIsolated(UInt128 pawns)
+    {
+        UInt128 rightExclude = pawns & BitboardConstants.RightEdgeExcludeMask;
+        UInt128 rightNeighbors = rightExclude << 1 | rightExclude << 11 | rightExclude >> 9;
+
+        UInt128 leftExclude = pawns & BitboardConstants.LeftEdgeExcludeMask;
+        UInt128 leftNeighbors = leftExclude >> 1 | leftExclude << 9 | leftExclude >> 11;
+
+        UInt128 nonIsolated = pawns & (leftNeighbors | rightNeighbors);
+        UInt128 isolated = pawns & ~nonIsolated;
+
+        return BitboardHelpers.CountBits(isolated);
+    }
+
+    private static int CountWhiteBackwards(UInt128 pawns, UInt128 enemyPawns)
+    {
+        UInt128 defendedPawns =
+            ((pawns & BitboardConstants.RightEdgeExcludeMask) << 9)
+            | ((pawns & BitboardConstants.LeftEdgeExcludeMask) << 11);
+        UInt128 undefendedPawns = pawns & ~defendedPawns;
+
+        UInt128 blockedPawns = enemyPawns >> 10;
+        undefendedPawns &= ~blockedPawns;
+
+        UInt128 adjacentFiles =
+            ((pawns & BitboardConstants.LeftEdgeExcludeMask) << 1)
+            | ((pawns & BitboardConstants.RightEdgeExcludeMask) >> 1);
+        undefendedPawns &= ~adjacentFiles;
+
+        UInt128 enemyAttacks =
+            ((enemyPawns & BitboardConstants.LeftEdgeExcludeMask) >> 9)
+            | ((enemyPawns & BitboardConstants.RightEdgeExcludeMask) >> 11);
+
+        UInt128 front = undefendedPawns << 10;
+        UInt128 backwardPawns = front & enemyAttacks;
+
+        return BitboardHelpers.CountBits(backwardPawns);
+    }
+
+    private static int CountBlackBackwards(UInt128 pawns, UInt128 enemyPawns)
+    {
+        UInt128 defendedPawns =
+            ((pawns & BitboardConstants.RightEdgeExcludeMask) >> 11)
+            | ((pawns & BitboardConstants.LeftEdgeExcludeMask) >> 9);
+        UInt128 undefendedPawns = pawns & ~defendedPawns;
+
+        UInt128 blockedPawns = enemyPawns << 10;
+        undefendedPawns &= ~blockedPawns;
+
+        UInt128 adjacentFiles =
+            ((pawns & BitboardConstants.LeftEdgeExcludeMask) << 1)
+            | ((pawns & BitboardConstants.RightEdgeExcludeMask) >> 1);
+        undefendedPawns &= ~adjacentFiles;
+
+        UInt128 enemyAttacks =
+            ((enemyPawns & BitboardConstants.LeftEdgeExcludeMask) << 11)
+            | ((enemyPawns & BitboardConstants.RightEdgeExcludeMask) << 9);
+
+        UInt128 front = undefendedPawns >> 10;
+        UInt128 backwardPawns = front & enemyAttacks;
+        return BitboardHelpers.CountBits(backwardPawns);
+    }
+
+    private static int CountWhitePassed(UInt128 pawns, UInt128 enemyPawns)
+    {
+        UInt128 mask = enemyPawns;
+        for (int file = 0; file < 10; file++)
+        {
+            mask |= mask >> 10;
+        }
+        mask &= (UInt128.One << 100) - 1;
+
+        UInt128 leftMask = (mask & BitboardConstants.LeftEdgeExcludeMask) >> 1;
+        UInt128 rightMask = (mask & BitboardConstants.RightEdgeExcludeMask) << 1;
+
+        UInt128 blocking = mask | leftMask | rightMask;
+        return BitboardHelpers.CountBits(pawns & ~blocking);
+    }
+
+    private static int CountBlackPassed(UInt128 pawns, UInt128 enemyPawns)
+    {
+        UInt128 mask = enemyPawns;
+        for (int file = 0; file < 10; file++)
+        {
+            mask |= mask << 10;
+        }
+        mask &= (UInt128.One << 100) - 1;
+
+        UInt128 leftMask = (mask & BitboardConstants.LeftEdgeExcludeMask) >> 1;
+        UInt128 rightMask = (mask & BitboardConstants.RightEdgeExcludeMask) << 1;
+
+        UInt128 blocking = mask | leftMask | rightMask;
+        return BitboardHelpers.CountBits(pawns & ~blocking);
+    }
+}
---
+++ backend/Tests/AnarchyChess.Ai.Tests/Tests/EvaluationTests/PawnStructureEvaluatorTests.cs
@@ -0,0 +1,276 @@
+﻿using AnarchyChess.Ai.Evaluation;
+using AnarchyChess.Api.TestInfrastructure.Factories;
+using AnarchyChess.EngineShared;
+using AwesomeAssertions;
+
+namespace AnarchyChess.Ai.Tests.Tests.EvaluationTests;
+
+public class PawnStructureEvaluatorTests
+{
+    [Fact]
+    public void Evaluate_returns_zero_on_empty_board()
+    {
+        BitBoard board = BitBoard.FromPieces([]);
+        PawnStructureEvaluator.Evaluate(board).Should().Be(0);
+    }
+
+    [Fact]
+    public void Evaluate_returns_zero_on_normal_structure()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("b4")] = PieceFactory.White(PieceType.Pawn),
+            [new("c5")] = PieceFactory.White(PieceType.Pawn),
+            [new("d5")] = PieceFactory.White(PieceType.Pawn),
+            [new("e5")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("g3")] = PieceFactory.White(PieceType.Pawn),
+            [new("h3")] = PieceFactory.White(PieceType.Pawn),
+            [new("i3")] = PieceFactory.White(PieceType.Pawn),
+            [new("j3")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("a8")] = PieceFactory.Black(PieceType.Pawn),
+            [new("b8")] = PieceFactory.Black(PieceType.Pawn),
+
+            [new("d7")] = PieceFactory.Black(PieceType.Pawn),
+            [new("e8")] = PieceFactory.Black(PieceType.Pawn),
+            [new("f7")] = PieceFactory.Black(PieceType.Pawn),
+            [new("g6")] = PieceFactory.Black(PieceType.Pawn),
+            [new("h7")] = PieceFactory.Black(PieceType.Pawn),
+            [new("i8")] = PieceFactory.Black(PieceType.Pawn),
+        };
+        BitBoard board = BitBoard.FromPieces(pieces);
+
+        PawnStructureEvaluator.Evaluate(board).Should().Be(0);
+    }
+
+    [Fact]
+    public void Evaluate_subtracts_opponent_score_when_white_to_move()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("b4")] = PieceFactory.White(PieceType.Pawn),
+            [new("c4")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("a7")] = PieceFactory.Black(PieceType.Pawn),
+            [new("a8")] = PieceFactory.Black(PieceType.Pawn),
+            [new("c7")] = PieceFactory.Black(PieceType.Pawn),
+        };
+        BitBoard board = BitBoard.FromPieces(pieces);
+
+        PawnStructureEvaluator
+            .Evaluate(board)
+            .Should()
+            .Be(
+                PawnStructureEvaluator.DoubledPenalty + (PawnStructureEvaluator.IsolatedPenalty * 3)
+            );
+    }
+
+    [Fact]
+    public void Evaluate_subtracts_opponent_score_when_black_to_move()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("b7")] = PieceFactory.Black(PieceType.Pawn),
+            [new("c7")] = PieceFactory.Black(PieceType.Pawn),
+
+            [new("a4")] = PieceFactory.White(PieceType.Pawn),
+            [new("a3")] = PieceFactory.White(PieceType.Pawn),
+            [new("c4")] = PieceFactory.White(PieceType.Pawn),
+        };
+        BitBoard board = BitBoard.FromPieces(pieces, isWhiteToMove: false);
+
+        PawnStructureEvaluator
+            .Evaluate(board)
+            .Should()
+            .Be(
+                PawnStructureEvaluator.DoubledPenalty + (PawnStructureEvaluator.IsolatedPenalty * 3)
+            );
+    }
+
+    [Fact]
+    public void Evaluate_includes_underage_pawns()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("d8")] = PieceFactory.Black(PieceType.Pawn),
+            [new("d7")] = PieceFactory.Black(PieceType.UnderagePawn),
+
+            [new("d2")] = PieceFactory.White(PieceType.Pawn),
+            [new("d3")] = PieceFactory.White(PieceType.UnderagePawn),
+            [new("d4")] = PieceFactory.White(PieceType.UnderagePawn),
+        };
+        BitBoard board = BitBoard.FromPieces(pieces);
+
+        PawnStructureEvaluator
+            .Evaluate(board)
+            .Should()
+            .Be(
+                //white
+                -(PawnStructureEvaluator.DoubledPenalty * 2)
+                    - (PawnStructureEvaluator.IsolatedPenalty * 3)
+                    // black
+                    + PawnStructureEvaluator.DoubledPenalty
+                    + (PawnStructureEvaluator.IsolatedPenalty * 2)
+            );
+    }
+
+    [Fact]
+    public void Evaluate_counts_doubled_pawns_correctly()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("a4")] = PieceFactory.White(PieceType.Pawn),
+            [new("a5")] = PieceFactory.White(PieceType.Pawn),
+            [new("b5")] = PieceFactory.White(PieceType.Pawn),
+            [new("a9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("b9")] = PieceFactory.Black(PieceType.Pawn),
+        };
+        BitBoard board = BitBoard.FromPieces(pieces);
+
+        PawnStructureEvaluator.Evaluate(board).Should().Be(-PawnStructureEvaluator.DoubledPenalty);
+    }
+
+    [Fact]
+    public void Evaluate_counts_isolated_pawns_correctly()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("a2")] = PieceFactory.White(PieceType.Pawn),
+            [new("j2")] = PieceFactory.White(PieceType.Pawn),
+            [new("d2")] = PieceFactory.White(PieceType.Pawn),
+            [new("g2")] = PieceFactory.White(PieceType.Pawn),
+            [new("h2")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("a9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("b9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("c9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("d9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("e9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("f9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("g9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("h9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("i9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("j9")] = PieceFactory.Black(PieceType.Pawn),
+        };
+        BitBoard board = BitBoard.FromPieces(pieces);
+
+        PawnStructureEvaluator
+            .Evaluate(board)
+            .Should()
+            .Be(-PawnStructureEvaluator.IsolatedPenalty * 3);
+    }
+
+    [Fact]
+    public void Evaluate_counts_neighboring_pawns_that_are_too_far_away_rankwise_isolated()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("e4")] = PieceFactory.White(PieceType.Pawn),
+            [new("f6")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("e9")] = PieceFactory.Black(PieceType.Pawn),
+            [new("f9")] = PieceFactory.Black(PieceType.Pawn),
+        };
+        BitBoard board = BitBoard.FromPieces(pieces);
+
+        PawnStructureEvaluator
+            .Evaluate(board)
+            .Should()
+            .Be(-PawnStructureEvaluator.IsolatedPenalty * 2);
+    }
+
+    [Fact]
+    public void Evaluate_counts_white_backwards_pawns()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("b4")] = PieceFactory.White(PieceType.Pawn),
+            [new("c4")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("e4")] = PieceFactory.White(PieceType.Pawn),
+            [new("f5")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("c6")] = PieceFactory.Black(PieceType.Pawn),
+            [new("d6")] = PieceFactory.Black(PieceType.Pawn),
+            [new("f7")] = PieceFactory.Black(PieceType.Pawn),
+            [new("g7")] = PieceFactory.Black(PieceType.Pawn),
+        };
+        BitBoard board = BitBoard.FromPieces(pieces);
+
+        PawnStructureEvaluator
+            .Evaluate(board)
+            .Should()
+            .Be(-PawnStructureEvaluator.BackwardsPenalty);
+    }
+
+    [Fact]
+    public void Evaluate_counts_black_backwards_pawns()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("g8")] = PieceFactory.Black(PieceType.Pawn),
+            [new("h8")] = PieceFactory.Black(PieceType.Pawn),
+
+            [new("e7")] = PieceFactory.Black(PieceType.Pawn),
+            [new("d6")] = PieceFactory.Black(PieceType.Pawn),
+
+            [new("c4")] = PieceFactory.White(PieceType.Pawn),
+            [new("d4")] = PieceFactory.White(PieceType.Pawn),
+            [new("f5")] = PieceFactory.White(PieceType.Pawn),
+            [new("g5")] = PieceFactory.White(PieceType.Pawn),
+        };
+        BitBoard board = BitBoard.FromPieces(pieces, isWhiteToMove: false);
+
+        PawnStructureEvaluator
+            .Evaluate(board)
+            .Should()
+            .Be(-PawnStructureEvaluator.BackwardsPenalty);
+    }
+
+    [Fact]
+    public void Evaluate_counts_white_passed_pawns()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("b4")] = PieceFactory.White(PieceType.Pawn),
+            [new("c5")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("e4")] = PieceFactory.White(PieceType.Pawn),
+            [new("f4")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("i7")] = PieceFactory.White(PieceType.Pawn),
+            [new("j7")] = PieceFactory.White(PieceType.Pawn),
+
+            [new("e7")] = PieceFactory.Black(PieceType.Pawn),
+            [new("f7")] = PieceFactory.Black(PieceType.Pawn),
+        };
+
+        BitBoard board = BitBoard.FromPieces(pieces);
+
+        PawnStructureEvaluator.Evaluate(board).Should().Be(PawnStructureEvaluator.PassedBonus * 4);
+    }
+
+    [Fact]
+    public void Evaluate_counts_black_passed_pawns()
+    {
+        Dictionary<AlgebraicPoint, Piece> pieces = new()
+        {
+            [new("e7")] = PieceFactory.Black(PieceType.Pawn),
+            [new("f7")] = PieceFactory.Black(PieceType.Pawn),
+
+            [new("b6")] = PieceFactory.Black(PieceType.Pawn),
+            [new("c5")] = PieceFactory.Black(PieceType.Pawn),
+
+            [new("i4")] = PieceFactory.Black(PieceType.Pawn),
+            [new("j4")] = PieceFactory.Black(PieceType.Pawn),
+
+            [new("e4")] = PieceFactory.White(PieceType.Pawn),
+            [new("f4")] = PieceFactory.White(PieceType.Pawn),
+        };
+
+        BitBoard board = BitBoard.FromPieces(pieces, isWhiteToMove: false);
+
+        PawnStructureEvaluator.Evaluate(board).Should().Be(PawnStructureEvaluator.PassedBonus * 4);
+    }
+}
