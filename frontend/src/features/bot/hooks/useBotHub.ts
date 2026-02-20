9fa3f2570ac7997cc572d37e8a0e0344fdb8a8e3
slomg <74960133+slomg@users.noreply.github.com>
2026-02-20 20:53:53 +0200

    Add bot hub hook

---
+++ frontend/src/features/bot/hooks/useBotHub.ts
@@ -0,0 +1,54 @@
+import useSignalREmitter from "@/features/signalR/hooks/useSignalREmitter";
+import useSignalREvent from "@/features/signalR/hooks/useSignalREvent";
+import { GameResultData, MoveSnapshot } from "@/lib/apiClient";
+import constants from "@/lib/constants";
+import { useMemo } from "react";
+
+export type BotClientEvents = {
+    PlayerMadeMoveAsync: [
+        move: MoveSnapshot,
+        plyNumber: number,
+        didMoveEndGame: boolean,
+    ];
+    BotMadeMoveAsync: [
+        move: MoveSnapshot,
+        plyNumber: number,
+        compressedLegalMoves: string,
+    ];
+    GameEndedAsync: [result: GameResultData];
+};
+
+type BotHubEvents = {
+    MakeMoveAsync: [moveKey: string];
+    ResignAsync: [];
+};
+
+export function useBotEvent<
+    TEventName extends Extract<keyof BotClientEvents, string>,
+>(
+    gameToken: string,
+    eventName: TEventName,
+    onEvent?: (...args: BotClientEvents[TEventName]) => void,
+) {
+    const url = useMemo(() => {
+        const u = new URL(constants.SIGNALR_PATHS.BOT);
+        u.searchParams.append("gameToken", gameToken);
+        return u.toString();
+    }, [gameToken]);
+
+    return useSignalREvent<BotClientEvents, TEventName>(
+        url,
+        eventName,
+        onEvent,
+    );
+}
+
+export function useGameEmitter(gameToken: string) {
+    const url = useMemo(() => {
+        const u = new URL(constants.SIGNALR_PATHS.BOT);
+        u.searchParams.append("gameToken", gameToken);
+        return u.toString();
+    }, [gameToken]);
+
+    return useSignalREmitter<BotHubEvents>(url);
+}
--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -68,6 +68,7 @@ const SIGNALR_PATHS = {
     LOBBY: `${process.env.NEXT_PUBLIC_API_URL}/api/hub/lobby`,
     OPENSEEK: `${process.env.NEXT_PUBLIC_API_URL}/api/hub/openseek`,
     GAME: `${process.env.NEXT_PUBLIC_API_URL}/api/hub/game`,
+    BOT: `${process.env.NEXT_PUBLIC_API_URL}/api/hub/bot`,
     CHALLENGE: `${process.env.NEXT_PUBLIC_API_URL}/api/hub/challenge`,
 };
 
