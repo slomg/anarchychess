92c22593cdbc70875d1ac99b89e212b4516a571e
slomg <74960133+slomg@users.noreply.github.com>
2026-02-20 17:02:50 +0200

    Update page.tsx

--- frontend/src/app/(public)/bot/page.tsx
+++ frontend/src/app/(public)/bot/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     ],
 };
 
-export default async function BotPage() {
+export default async function BotPlayPage() {
     return (
         <StaticChessboardWithSidebar
             prioritizeAside
