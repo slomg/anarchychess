1e6b4fd6934461df4e8ac613d29b8b8cdabc4e99
slomg <74960133+slomg@users.noreply.github.com>
2026-02-20 12:15:33 +0200

    Add bot controller

---
+++ backend/Tests/AnarchyChess.Api.Functional/Tests/AnarchyBotTests/AnarchyBotControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using AnarchyChess.Api.AnarchyBot.Grains;
+using AnarchyChess.Api.Profile.Models;
+using AnarchyChess.Api.TestInfrastructure;
+using AnarchyChess.EngineShared;
+using AwesomeAssertions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AnarchyChess.Api.Functional.Tests.AnarchyBotTests;
+
+public class AnarchyBotControllerTests : BaseFunctionalTest
+{
+    private readonly IGrainFactory _grains;
+
+    public AnarchyBotControllerTests(AnarchyChessWebApplicationFactory factory)
+        : base(factory)
+    {
+        _grains = Scope.ServiceProvider.GetRequiredService<IGrainFactory>();
+    }
+
+    [Fact]
+    public async Task StartBotGame_starts_bot_game_for_guest()
+    {
+        var guest = UserId.Guest();
+        AuthUtils.AuthenticateGuest(ApiClient, guest);
+
+        var response = await ApiClient.Api.StartAnarchyBotGameAsync(myColor: GameColor.White);
+
+        response.IsSuccessful.Should().BeTrue();
+        response.Content.Should().NotBeNull();
+        string gameToken = response.Content;
+
+        var grain = _grains.GetGrain<IAnarchyBotGrain>(gameToken);
+        var stateResult = await grain.GetStateAsync(CT);
+        stateResult.IsError.Should().BeFalse();
+        var state = stateResult.Value;
+
+        state.WhitePlayer.UserId.Should().Be(guest);
+        state.WhitePlayer.UserName.Should().Be("Guest");
+        state.WhitePlayer.CountryCode.Should().Be("XX");
+
+        state.BlackPlayer.UserId.Should().Be(UserId.AnarchyBot());
+    }
+
+    [Fact]
+    public async Task StartBotGame_starts_bot_game_for_authed()
+    {
+        var user = (await AuthUtils.AuthenticateAsync(ApiClient)).User;
+
+        var response = await ApiClient.Api.StartAnarchyBotGameAsync(myColor: GameColor.White);
+
+        response.IsSuccessful.Should().BeTrue();
+        response.Content.Should().NotBeNull();
+        string gameToken = response.Content;
+
+        var grain = _grains.GetGrain<IAnarchyBotGrain>(gameToken);
+        var stateResult = await grain.GetStateAsync(CT);
+        stateResult.IsError.Should().BeFalse();
+        var state = stateResult.Value;
+
+        state.WhitePlayer.UserId.Should().Be(user.Id);
+        state.WhitePlayer.UserName.Should().Be(user.UserName);
+        state.WhitePlayer.CountryCode.Should().Be(user.CountryCode);
+
+        state.BlackPlayer.UserId.Should().Be(UserId.AnarchyBot());
+    }
+}
--- backend/Tests/AnarchyChess.Api.TestInfrastructure/IAnarchyChessApi.cs
+++ backend/Tests/AnarchyChess.Api.TestInfrastructure/IAnarchyChessApi.cs
@@ -1,4 +1,5 @@
 ﻿using AnarchyChess.Api.Analysis.Models;
+using AnarchyChess.Api.AnarchyBot.Models;
 using AnarchyChess.Api.ArchivedGames.Models;
 using AnarchyChess.Api.Challenges.Models;
 using AnarchyChess.Api.GameSnapshot.Models;
@@ -9,6 +10,7 @@ using AnarchyChess.Api.Profile.DTOs;
 using AnarchyChess.Api.Quests.DTOs;
 using AnarchyChess.Api.Shared.Models;
 using AnarchyChess.Api.UserRating.Models;
+using AnarchyChess.EngineShared;
 using Refit;
 
 namespace AnarchyChess.Api.TestInfrastructure;
@@ -181,5 +183,13 @@ public interface IAnarchyChessApi
 
     [Delete("/api/challenge/incoming")]
     Task<IApiResponse> CancelAllIncomingChallengesAsync();
+    #endregion
+
+    #region Anarchy Bot
+    [Get("/api/anarchybot/{gameToken}")]
+    Task<IApiResponse<AnarchyBotGameState>> GetAnarchyBotGameAsync(string gameToken);
+
+    [Post("/api/anarchybot/start")]
+    Task<IApiResponse<string>> StartAnarchyBotGameAsync(GameColor myColor);
     #endregion
 }
