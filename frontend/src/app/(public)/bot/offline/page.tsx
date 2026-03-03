e1d71e86eed2a6ea306c67030ac7ba11279ce097
slomg <74960133+slomg@users.noreply.github.com>
2026-03-03 15:47:24 +0200

    Add title to offline page

--- frontend/src/app/(public)/bot/offline/page.tsx
+++ frontend/src/app/(public)/bot/offline/page.tsx
@@ -1,5 +1,7 @@
 import BotOffline from "@/features/bot/components/BotOffline";
 
+export const metadata = { title: "Anarchy Bot Offline - Anarchy Chess" };
+
 export default function BotOfflinePage() {
     return <BotOffline />;
 }
