72582f461faedda1e239a852f7048a5e645b2031
slomg <74960133+slomg@users.noreply.github.com>
2026-03-07 15:25:42 +0200

    Fix todo

--- backend/AnarchyChess.Ai.Magic/PiecesMagic/TwoStraightSquares.cs
+++ backend/AnarchyChess.Ai.Magic/PiecesMagic/TwoStraightSquares.cs
@@ -7,14 +7,14 @@ public sealed class TwoStraightSquares : PieceMagic
     public override string Name => "TwoStraightSquares";
 
     public override UInt128 GenerateMask(AlgebraicPoint point) =>
-        SlideMask(TODO, new Offset(X: 1, Y: 0), limit: 2)
-        | SlideMask(TODO, new Offset(X: -1, Y: 0), limit: 2)
-        | SlideMask(TODO, new Offset(X: 0, Y: 1), limit: 2)
-        | SlideMask(TODO, new Offset(X: 0, Y: -1), limit: 2);
+        SlideMask(point, new Offset(X: 1, Y: 0), limit: 2)
+        | SlideMask(point, new Offset(X: -1, Y: 0), limit: 2)
+        | SlideMask(point, new Offset(X: 0, Y: 1), limit: 2)
+        | SlideMask(point, new Offset(X: 0, Y: -1), limit: 2);
 
     public override UInt128 ComputeAttacks(AlgebraicPoint point, UInt128 blocker) =>
-        SlideAttack(TODO, new Offset(X: 1, Y: 0), blocker, limit: 2)
-        | SlideAttack(TODO, new Offset(X: -1, Y: 0), blocker, limit: 2)
-        | SlideAttack(TODO, new Offset(X: 0, Y: 1), blocker, limit: 2)
-        | SlideAttack(TODO, new Offset(X: 0, Y: -1), blocker, limit: 2);
+        SlideAttack(point, new Offset(X: 1, Y: 0), blocker, limit: 2)
+        | SlideAttack(point, new Offset(X: -1, Y: 0), blocker, limit: 2)
+        | SlideAttack(point, new Offset(X: 0, Y: 1), blocker, limit: 2)
+        | SlideAttack(point, new Offset(X: 0, Y: -1), blocker, limit: 2);
 }
