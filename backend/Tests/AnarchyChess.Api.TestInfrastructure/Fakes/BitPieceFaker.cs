738c2056afe3da3db806d0bf39d64a59fbb8561f
slomg <74960133+slomg@users.noreply.github.com>
2026-03-12 14:48:46 +0200

    Add bit piece faker

---
+++ backend/Tests/AnarchyChess.Api.TestInfrastructure/Fakes/BitPieceFaker.cs
@@ -0,0 +1,14 @@
+﻿using AnarchyChess.Ai.Models;
+using AnarchyChess.EngineShared;
+
+namespace AnarchyChess.Api.TestInfrastructure.Fakes;
+
+public class BitPieceFaker : StructFaker<BitPiece>
+{
+    public BitPieceFaker()
+    {
+        StrictMode(true);
+        RuleFor(x => x.Type, f => f.PickRandom<PieceType>());
+        RuleFor(x => x.Color, f => f.PickRandom<BitPieceColor>());
+    }
+}
