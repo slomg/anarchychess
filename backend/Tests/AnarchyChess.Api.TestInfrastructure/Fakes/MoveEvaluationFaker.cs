e4be95343b4d2140b55e148a36daa96e02a3be32
slomg <74960133+slomg@users.noreply.github.com>
2026-03-12 14:48:57 +0200

    Add move evaluation faker

---
+++ backend/Tests/AnarchyChess.Api.TestInfrastructure/Fakes/MoveEvaluationFaker.cs
@@ -0,0 +1,13 @@
+﻿using AnarchyChess.Ai.Models;
+
+namespace AnarchyChess.Api.TestInfrastructure.Fakes;
+
+public class MoveEvaluationFaker : StructFaker<MoveEvaluation>
+{
+    public MoveEvaluationFaker()
+    {
+        StrictMode(true);
+        RuleFor(x => x.Move, f => new BitMoveFaker().Generate());
+        RuleFor(x => x.EvalForBot, f => f.Random.Number(min: -10000, max: 10000));
+    }
+}
