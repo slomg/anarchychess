edc64c1f6b2132fe6c4451a4fde2060be4660827
slomg <74960133+slomg@users.noreply.github.com>
2026-03-12 14:48:52 +0200

    Add bit move faker

---
+++ backend/Tests/AnarchyChess.Api.TestInfrastructure/Fakes/BitMoveFaker.cs
@@ -0,0 +1,19 @@
+﻿using AnarchyChess.Ai.Models;
+using AnarchyChess.EngineShared;
+
+namespace AnarchyChess.Api.TestInfrastructure.Fakes;
+
+public class BitMoveFaker : StructFaker<BitMove>
+{
+    public BitMoveFaker()
+    {
+        StrictMode(true);
+        RuleFor(x => x.From, f => f.Random.Number(min: 0, max: 100));
+        RuleFor(x => x.To, f => f.Random.Number(min: 0, max: 100));
+        RuleFor(x => x.Piece, f => new BitPieceFaker().Generate());
+        RuleFor(x => x.CapturesMask, UInt128.Zero);
+        RuleFor(x => x.PromotesTo, (PieceType?)null);
+        RuleFor(x => x.ForcedMovePriority, ForcedMovePriority.None);
+        RuleFor(x => x.SpecialMoveType, SpecialMoveType.None);
+    }
+}
