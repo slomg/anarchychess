6a9eb7fde5026c29eab3ea6d176b2ce88a64f056
slomg <74960133+slomg@users.noreply.github.com>
2026-03-24 11:17:55 +0200

    Add serializer

--- backend/AnarchyChess.Api/GameLogic/Models/MoveStun.cs
+++ backend/AnarchyChess.Api/GameLogic/Models/MoveStun.cs
@@ -2,4 +2,6 @@
 
 namespace AnarchyChess.Api.GameLogic.Models;
 
+[GenerateSerializer]
+[Alias("AnarchyChess.Api.GameLogic.Models.MoveStun")]
 public record MoveStun(AlgebraicPoint Position, Piece Piece, int StunForTurns);
