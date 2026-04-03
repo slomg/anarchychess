d6a55808efc3b86278ec21a07a5022316d2c0b43
slomg <74960133+slomg@users.noreply.github.com>
2026-04-03 11:58:25 +0300

    Remove incorrect test

--- frontend/src/features/chessboard/stores/__tests__/uiLegalMovesSlice.test.ts
+++ frontend/src/features/chessboard/stores/__tests__/uiLegalMovesSlice.test.ts
@@ -92,26 +92,6 @@ describe("UiLegalMovesSlice", () => {
             expect(result).toEqual(triggerMove);
         });
 
-        it("should return a move that matches via trigger even if trigger wasn't used", async () => {
-            const origin = logicalPoint({ x: 4, y: 4 });
-            const trigger = logicalPoint({ x: 6, y: 6 });
-            const dest = logicalPoint({ x: 9, y: 9 });
-            const piece = createFakePiece({ position: origin });
-            const pieces = BoardPieces.fromPieces(piece);
-
-            const triggerMove = createFakeMove({
-                from: origin,
-                to: dest,
-                triggers: [trigger],
-            });
-            const legalMoves = new LegalMoves([triggerMove]);
-            const { setLatestLegalMoves, getLegalMove } = store.getState();
-            setLatestLegalMoves(legalMoves);
-
-            const result = await getLegalMove(dest, piece.id, pieces);
-            expect(result).toEqual(triggerMove);
-        });
-
         it("should return null if multiple moves match but promotion is cancelled", async () => {
             const origin = logicalPoint({ x: 0, y: 1 });
             const dest = logicalPoint({ x: 0, y: 7 });
