1c4d611a95632577be8ec6f9629435991c062dc7
slomg <74960133+slomg@users.noreply.github.com>
2026-04-12 11:23:02 +0300

    Remove test log

--- backend/AnarchyChess.Api/Game/Models/LegalMoveSet.cs
+++ backend/AnarchyChess.Api/Game/Models/LegalMoveSet.cs
@@ -25,11 +25,6 @@ public record LegalMoveSet(
                 moveCaptureMask |= UInt128.One << capture.Position.AsIdx();
             }
 
-            if (move.From.AsIdx() == botMove.From && move.To.AsIdx() == botMove.To)
-            {
-                Console.WriteLine("");
-            }
-
             return move.From.AsIdx() == botMove.From
                 && move.To.AsIdx() == botMove.To
                 && move.PromotesTo == botMove.PromotesTo
