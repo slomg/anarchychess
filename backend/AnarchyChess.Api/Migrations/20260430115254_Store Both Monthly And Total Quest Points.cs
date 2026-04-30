4a37c944596b2c79211c4db55317ba4ab60a1266
slomg <74960133+slomg@users.noreply.github.com>
2026-04-30 15:16:52 +0300

    Add total points to entity

---
+++ backend/AnarchyChess.Api/Migrations/20260430115254_Store Both Monthly And Total Quest Points.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnarchyChess.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class StoreBothMonthlyAndTotalQuestPoints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "points",
+                table: "quest_points",
+                newName: "monthly_points"
+            );
+
+            migrationBuilder.AddColumn<int>(
+                name: "total_points",
+                table: "quest_points",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "total_points", table: "quest_points");
+
+            migrationBuilder.RenameColumn(
+                name: "monthly_points",
+                table: "quest_points",
+                newName: "points"
+            );
+        }
+    }
+}
