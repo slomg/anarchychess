b12882ee1254b300e883e8eae930d70b0c981c66
slomg <74960133+slomg@users.noreply.github.com>
2026-05-02 12:14:03 +0300

    Create underage pawn bishop capture quest

---
+++ backend/AnarchyChess.Api/QuestLogic/QuestDefinitions/UnderagePawnBishopCaptureQuest.cs
@@ -0,0 +1,32 @@
+﻿using AnarchyChess.Api.QuestLogic.Models;
+using AnarchyChess.Api.QuestLogic.MoveConditions;
+using AnarchyChess.Api.QuestLogic.QuestMetrics;
+using AnarchyChess.EngineShared;
+
+namespace AnarchyChess.Api.QuestLogic.QuestDefinitions;
+
+public sealed class UnderagePawnBishopCaptureQuest : IQuestDefinition
+{
+    public IEnumerable<QuestVariant> Variants =>
+        [
+            CreateVariant(3, QuestDifficulty.Easy),
+            CreateVariant(6, QuestDifficulty.Medium),
+            CreateVariant(9, QuestDifficulty.Hard),
+        ];
+
+    private static QuestVariant CreateVariant(int pawns, QuestDifficulty difficulty) =>
+        new(
+            Description: $"Capture {pawns} underage pawns with your bishop",
+            Difficulty: difficulty,
+            Target: pawns,
+            Conditions: () => [],
+            Progressors: () =>
+
+                [
+                    new OwnMoveCountMetric(
+                        new IsMoveOfPiece(PieceType.Bishop),
+                        new IsMoveCaptureOf(PieceType.UnderagePawn)
+                    ),
+                ]
+        );
+}
