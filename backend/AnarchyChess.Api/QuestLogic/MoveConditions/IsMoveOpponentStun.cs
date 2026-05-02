fbf22f44fce20adda783565e3035630f966cc1b8
slomg <74960133+slomg@users.noreply.github.com>
2026-05-02 11:56:53 +0300

    Add missing serializer

--- backend/AnarchyChess.Api/QuestLogic/MoveConditions/IsMoveOpponentStun.cs
+++ backend/AnarchyChess.Api/QuestLogic/MoveConditions/IsMoveOpponentStun.cs
@@ -2,6 +2,8 @@
 
 namespace AnarchyChess.Api.QuestLogic.MoveConditions;
 
+[GenerateSerializer]
+[Alias("AnarchyChess.Api.QuestLogic.MoveConditions.IsMoveOpponentStun")]
 public sealed class IsMoveOpponentStun : IMoveCondition
 {
     public bool Evaluate(Move move) => move.Stuns.Any(x => x.Piece.Color != move.Piece.Color);
